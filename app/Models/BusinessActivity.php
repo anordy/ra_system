6d5cb2bd0caac81f34857ab8aec96c9d34a9e4c1
Meshack Fungo <meshack.fungo@ubx.co.tz>
2022-06-21 18:23:19 +0300

    Update tables and models for business activities and tax types

--- app/Models/BusinessActivity.php
+++ app/Models/BusinessActivity.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class TaxType extends Model
+class BusinessActivity extends Model
 {
     use HasFactory, SoftDeletes;
 }
