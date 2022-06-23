aa5ca69902955a8dd39f03a67c398a9cd6d49522
LuksfordI <Luksford.Isack@ubx.co.tz>
2022-06-23 20:29:35 +0300

    migration

---
+++ database/migrations/2022_06_23_125229_create_tax_agent_academic_qualifications_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaxAgentAcademicQualificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tax_agent_academic_qualifications', function (Blueprint $table) {
+            $table->id();
+			$table->string('school_name');
+			$table->date('from');
+			$table->date('to');
+			$table->string('examining_body');
+			$table->integer('division_id');
+			$table->unsignedBigInteger('taxpayer_id');
+			$table->foreign('taxpayer_id')->references('id')->on('taxpayers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tax_agent_academic_qualifications');
+    }
+}
