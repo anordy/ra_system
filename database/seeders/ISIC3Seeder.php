1a9e722ac55af14a77b964ec4f24e83536272bd8
Kedmon Maquiz <43686770+kedmonmaquiz@users.noreply.github.com>
2022-07-07 19:29:00 +0300

    isics seeder

---
+++ database/seeders/ISIC3Seeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Imports\ISIC3Import;
+use Exception;
+use Illuminate\Database\Seeder;
+
+class ISIC3Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        try{
+            $import = new ISIC3Import;
+            $import->import(public_path('isics/ISIC_LEVEL_3.xlsx'));
+        }catch(Exception $e){
+            echo $e->getMessage();
+        }
+    }
+}
