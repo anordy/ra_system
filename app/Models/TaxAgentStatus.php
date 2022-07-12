dd3503be084351f7480b809df0cc65a1b184b504
Kedmon Maquiz <43686770+kedmonmaquiz@users.noreply.github.com>
2022-07-12 09:26:14 +0300

    minor fixes

--- app/Models/TaxAgentStatus.php
+++ app/Models/TaxAgentStatus.php
@@ -9,9 +9,9 @@ class TaxAgentStatus extends Model
 {
     use HasFactory;
 
-    public const DRAFTING = 'drafting';
-    public const PENDING = 'pending';
-    public const APPROVED = 'approved';
-    public const REJECTED = 'rejected';
+    public const DRAFTING =  'drafting';
+    public const PENDING =   'pending';
+    public const APPROVED =  'approved';
+    public const REJECTED =  'rejected';
     public const COMPLETED = 'completed';
 }
