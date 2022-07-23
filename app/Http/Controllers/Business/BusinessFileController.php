afa65060cd5bbfc7c1c09f6651f2b2e96caec510
Meshack Fungo <meshack.fungo@ubx.co.tz>
2022-07-23 10:02:02 +0300

    Update Tin Viewing

--- app/Http/Controllers/Business/BusinessFileController.php
+++ app/Http/Controllers/Business/BusinessFileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Business;
 use App\Http\Controllers\Controller;
 use App\Models\Business;
 use App\Models\BusinessFile;
+use App\Models\Taxpayer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
@@ -25,6 +26,17 @@ class BusinessFileController extends Controller
         }
 
         // If they dont meet requirements, abort
+        return abort(404);
+    }
+
+    public function getTinFile($taxpayerId)
+    {
+        $taxpayer = Taxpayer::findOrFail(decrypt($taxpayerId));
+
+        if ($taxpayer->tin_location){
+            return Storage::response($taxpayer->tin_location);
+        }
+
         return abort(404);
     }
 }
