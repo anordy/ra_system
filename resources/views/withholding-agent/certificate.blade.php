0a676c916249aa61ace7d7fe4be1fb4a11e09b6a
mangerere <107620925+mangerere@users.noreply.github.com>
2022-07-24 21:45:40 +0300

    Update certificate.blade.php

--- resources/views/withholding-agent/certificate.blade.php
+++ resources/views/withholding-agent/certificate.blade.php
@@ -2,10 +2,10 @@
 <html>
 
 <head>
-    <title>Hi</title>
+    <title></title>
     <style>
         body {
-            background-image: url("{{ public_path()}}/images/certificate/withholding_agent.jpg");
+            background-image: url("{{ public_path() }}/images/certificate/withholding_agent.jpg");
             background-repeat: no-repeat;
             background-position: center;
             background-size: cover;
@@ -28,15 +28,43 @@
             text-transform: uppercase;
             font-weight: bold
         }
+        .startDate {
+            position: absolute;
+            font-size: 1.2em;
+            top: 75%;
+            left: 43%;
+            text-transform: uppercase;
+            font-weight: bold
+        }
+
+        .dateGiven {
+            position: absolute;
+            font-size: 1.2em;
+            top: 80%;
+            left: 43%;
+            text-transform: uppercase;
+            font-weight: bold
+        }
+
+        .location {
+            position: absolute;
+            font-size: 1.2em;
+            top: 46.5%;
+            left: 45%;
+            text-transform: uppercase;
+            font-weight: bold
+        }
     </style>
 </head>
 
 <body>
 
+    <p class="watermark">Online Copy</p>
     <span class="taxpayerName">{{ $wa_responsible_person->taxpayer->full_name ?? '' }}</span>
+    <span class="location">{{ $wa_responsible_person->taxpayer->location ?? '' }}</span>
     <span class="witholdingAgentNumber">{{ $wa_responsible_person->withholdingAgent->wa_number ?? '' }}</span>
-
-   
+    <span class="startDate">{{ $wa_responsible_person->created_at->toFormattedDateString() ?? '' }}</span>
+    <span class="dateGiven">{{ date('M d, Y') }}</span>
 </body>
 
 </html>
