6c1bceb8af239f42a7f01aa6e5c9ae01de4cea09
mangerere <107620925+mangerere@users.noreply.github.com>
2022-07-24 12:43:57 +0300

    Minor changes

--- resources/views/business/updates/action.blade.php
+++ resources/views/business/updates/action.blade.php
@@ -1,2 +1,9 @@
-<a href="{{ route('business.showRequest', encrypt($row->id)) }}" class="btn btn-outline-info rounded btn-sm"> <i
-        class="bi bi-eye-fill mr-1"></i> View & Approve </a>
+@if ($row->status === \App\Models\BusinessStatus::PENDING)
+    <a href="{{ route('business.showRequest', encrypt($row->id)) }}" class="btn btn-outline-primary btn-sm">
+        <i class="bi bi-eye-fill mr-1"></i> View & Approve
+    </a>
+@else
+    <a href="{{ route('business.showRequest', encrypt($row->id)) }}" class="btn btn-outline-primary btn-sm">
+        <i class="bi bi-eye-fill mr-1"></i> View
+    </a>
+@endif
