5b211f4ac2f2ea50718fe50a0a5766de0851433c
Meshack Fungo <meshack.fungo@ubx.co.tz>
2022-07-26 16:24:09 +0300

    Add business certificate registration

--- resources/views/business/registrations/show.blade.php
+++ resources/views/business/registrations/show.blade.php
@@ -4,6 +4,13 @@
 
 @section('content')
     <div class="mt-3">
+
+    <div class="d-flex justify-content-end mb-3">
+        <a target="_blank" href="{{ route('business.certificate', encrypt($business->id)) }}" class="btn btn-success btn-sm mt-1">
+            <i class="bi bi-patch-check mr-1"></i> Certificate of Registration
+        </a>
+    </div>
+
         @include('business.registrations.includes.business_info')
     </div>
 @endsection
