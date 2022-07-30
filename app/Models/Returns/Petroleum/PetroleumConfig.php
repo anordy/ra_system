be1878229219b719ca7afe1b4ade2ec66f716e6d
gozbert <Gozbert.Stanslaus@ubx.co.tz>
2022-07-30 18:26:30 +0300

    petroleum config and items

--- app/Models/Returns/Petroleum/PetroleumConfig.php
+++ app/Models/Returns/Petroleum/PetroleumConfig.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Returns\Petroleum;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PetroleumReturnItem extends Model
+class PetroleumConfig extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
 }
--- app/Models/Returns/Petroleum/PetroleumReturn.php
+++ app/Models/Returns/Petroleum/PetroleumReturn.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\Returns;
+namespace App\Models\Returns\Petroleum;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
