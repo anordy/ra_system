667fd37f2d75d89b61401edd86f7c38b68a9ae61
anordy <anordypatrick98@gmail.com>
2022-07-30 15:54:18 +0300

    change table

--- database/migrations/2022_07_29_183743_create_port_returns_table.php
+++ database/migrations/2022_07_29_183743_create_port_returns_table.php
@@ -15,6 +15,15 @@ class CreatePortReturnsTable extends Migration
     {
         Schema::create('port_returns', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('location_id');
+            $table->string('return_month_id');
+            $table->string('taxtype_code');
+            $table->decimal('total_input_tax', 40, 2);
+            $table->decimal('total_vat_payable_tzs', 40, 2);
+            $table->decimal('total_vat_payable_usd', 40, 2);
+            $table->decimal('infrastructure', 40, 2);
+            $table->decimal('infrastructure_znz_znz', 40, 2);
+            $table->decimal('infrastructure_znz_tm', 40, 2);
             $table->timestamps();
         });
     }
---
+++ database/migrations/2022_07_30_134045_create_port_return_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePortReturnItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('port_return_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('port_return_id');
+            $table->unsignedBigInteger('port_return_config_id');
+            $table->decimal('input_amount', 40, 2);
+            $table->decimal('vat_amount', 40, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('port_return_items');
+    }
+}
