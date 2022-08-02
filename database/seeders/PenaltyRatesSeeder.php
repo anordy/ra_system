8707a1e9ea1021f1da5a2e91fcbfb9c84c13884c
mangerere <107620925+mangerere@users.noreply.github.com>
2022-08-02 16:41:32 +0300

    Update PenaltyRatesSeeder.php

--- database/seeders/PenaltyRatesSeeder.php
+++ database/seeders/PenaltyRatesSeeder.php
@@ -18,6 +18,7 @@ class PenaltyRatesSeeder extends Seeder
             ['financial_year_id'=>2,'code'=> 'LF', 'name' => 'Late Filling', 'rate' => 0.1],
             ['financial_year_id'=>2,'code'=> 'LPB', 'name' => 'Late Payment Before', 'rate' => 0.2],
             ['financial_year_id'=>2,'code'=> 'LPA', 'name' => 'Late Payment After', 'rate' => 0.1],
+            ['financial_year_id'=>2,'code'=> 'WEG', 'name' => 'Which Ever Greater', 'rate' => 100000],
         ];
 
         foreach($rates as $item){
