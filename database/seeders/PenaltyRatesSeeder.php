898d04a62bc27811632836f53f1111cddc16d709
Meshack Fungo <meshack.fungo@ubx.co.tz>
2022-08-03 09:13:51 +0300

    Update PenaltyRatesSeeder.php

--- database/seeders/PenaltyRatesSeeder.php
+++ database/seeders/PenaltyRatesSeeder.php
@@ -15,15 +15,18 @@ class PenaltyRatesSeeder extends Seeder
     public function run()
     {
         $rates = [
-            ['financial_year_id'=>1,'code'=> 'LF', 'name' => 'Late Filling', 'rate' => 0.1],
-            ['financial_year_id'=>1,'code'=> 'LPB', 'name' => 'Late Payment Before', 'rate' => 0.1],
-            ['financial_year_id'=>1,'code'=> 'LPA', 'name' => 'Late Payment After', 'rate' => 0.1],
-            ['financial_year_id'=>2,'code'=> 'LF', 'name' => 'Late Filling', 'rate' => 0.1],
-            ['financial_year_id'=>2,'code'=> 'LPB', 'name' => 'Late Payment Before', 'rate' => 0.2],
-            ['financial_year_id'=>2,'code'=> 'LPA', 'name' => 'Late Payment After', 'rate' => 0.1],
-            ['financial_year_id'=>3,'code'=> 'LF', 'name' => 'Late Filling', 'rate' => 0.1],
-            ['financial_year_id'=>3,'code'=> 'LPB', 'name' => 'Late Payment Before', 'rate' => 0.3],
-            ['financial_year_id'=>3,'code'=> 'LPA', 'name' => 'Late Payment After', 'rate' => 0.1],
+            ['financial_year_id'=> 1, 'code'=> 'LF', 'name' => 'Late Filling', 'rate' => 0.1],
+            ['financial_year_id'=> 1, 'code'=> 'LPB', 'name' => 'Late Payment Before', 'rate' => 0.1],
+            ['financial_year_id'=> 1, 'code'=> 'LPA', 'name' => 'Late Payment After', 'rate' => 0.1],
+            ['financial_year_id'=> 1, 'code'=> 'WEG', 'name' => 'Which Ever Greater', 'rate' => 100000],
+            ['financial_year_id'=> 2, 'code'=> 'LF', 'name' => 'Late Filling', 'rate' => 0.1],
+            ['financial_year_id'=> 2, 'code'=> 'LPB', 'name' => 'Late Payment Before', 'rate' => 0.2],
+            ['financial_year_id'=> 2, 'code'=> 'LPA', 'name' => 'Late Payment After', 'rate' => 0.1],
+            ['financial_year_id'=> 2, 'code'=> 'WEG', 'name' => 'Which Ever Greater', 'rate' => 100000],
+            ['financial_year_id'=> 3, 'code'=> 'LF', 'name' => 'Late Filling', 'rate' => 0.1],
+            ['financial_year_id'=> 3, 'code'=> 'LPB', 'name' => 'Late Payment Before', 'rate' => 0.2],
+            ['financial_year_id'=> 3, 'code'=> 'LPA', 'name' => 'Late Payment After', 'rate' => 0.1],
+            ['financial_year_id'=> 3, 'code'=> 'WEG', 'name' => 'Which Ever Greater', 'rate' => 100000]
         ];
 
         foreach($rates as $item){
