77e119c905f31e3454d8d4c9240b24f6f69b4388
Meshack Fungo <meshack.fungo@ubx.co.tz>
2022-08-04 17:59:58 +0300

    Minor updates

--- app/Models/TaxType.php
+++ app/Models/TaxType.php
@@ -12,23 +12,27 @@ class TaxType extends Model implements Auditable
 {
     use HasFactory, SoftDeletes, \OwenIt\Auditing\Auditable;
 
-    public const VAT               = 'vat';
-    public const HOTEL             = 'hotel-levy';
-    public const RESTAURANT        = 'restaurant-levy';
-    public const TOUR              = 'tour-operator-levy';
-    public const LAND              = 'land-lease';
-    public const PUBLIC_SERVICE    = 'public-service';
-    public const EXCISE_DUTY       = 'excise-duty';
-    public const PETROLEUM         = 'petroleum-levy';
-    public const AIRPORT_SERVICE   = 'airport-service';
-    public const AIRPORT_SAFETY    = 'airport-safety';
-    public const SEAPORT_SERVICE   = 'seaport-service';
+    public const VAT = 'vat';
+    public const HOTEL = 'hotel-levy';
+    public const RESTAURANT = 'restaurant-levy';
+    public const TOUR = 'tour-operator-levy';
+    public const LAND = 'land-lease';
+    public const PUBLIC_SERVICE = 'public-service';
+    public const EXCISE_DUTY = 'excise-duty';
+    public const PETROLEUM = 'petroleum-levy';
+    public const AIRPORT_SERVICE = 'airport-service';
+    public const AIRPORT_SAFETY = 'airport-safety';
+    public const SEAPORT_SERVICE = 'seaport-service';
     public const SEAPORT_TRANSPORT = 'seaport-transport';
-    public const TAX_CONSULTANT    = 'tax-consultant';
-    public const STAMP_DUTY        = 'stamp-duty';
-    public const LUMPSUM_PAYMENT   = 'lumpsum-payment';
-    public const ELECTRONIC_MONEY_TRANSACTION   = 'electronic-money-transaction';
-    public const MOBILE_MONEY_TRANSFER   = 'mobile-money-transfer';
+    public const TAX_CONSULTANT = 'tax-consultant';
+    public const STAMP_DUTY = 'stamp-duty';
+    public const LUMPSUM_PAYMENT = 'lumpsum-payment';
+    public const ELECTRONIC_MONEY_TRANSACTION = 'electronic-money-transaction';
+    public const MOBILE_MONEY_TRANSFER = 'mobile-money-transfer';
+    public const PENALTY = 'penalty';
+    public const INFRASTRUCTURE = 'infrastructure';
+    public const RDF = 'road-development-fund';
+    public const ROAD_LICENSE_FEE = 'road-license-fee';
 
     protected $fillable = [
         'name',
@@ -36,9 +40,9 @@ class TaxType extends Model implements Auditable
 
     public function landLeases()
     {
-        return $this->hasMany(LandLease::class,'taxpayer_id');
+        return $this->hasMany(LandLease::class, 'taxpayer_id');
     }
-    
+
     public function vatReturn()
     {
         return $this->hasOne(VatReturn::class, 'taxtype_code', 'code');
