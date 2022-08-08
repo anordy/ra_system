e54803134e120bec5df3357088888a34baf56de9
mangerere <107620925+mangerere@users.noreply.github.com>
2022-08-08 15:12:48 +0300

    Major changes

---
+++ app/Jobs/Business/Branch/SendBranchCorrectionSMS.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs\Business\Branch;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Http\Controllers\v1\SMSController;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class SendBranchCorrectionSMS implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $payload;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($payload)
+    {
+        $this->payload = $payload;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $sms_controller = new SMSController;
+        $send_to = $this->payload['branch']->taxpayer->mobile;
+        $source = config('modulesconfig.smsheader');
+        $customer_message = "Your ZRB branch registration for {$this->payload['branch']->name} has corrections. Please log in to view more information.";
+        $sms_controller->sendSMS($send_to, $source, $customer_message);
+    }
+}
---
+++ resources/views/emails/business/branch/approved.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Hello {{ $payload['branch']->taxpayer->first_name }} {{ $payload['branch']->taxpayer->last_name }},
+
+Your ZRB new branch registration with name {{ $payload['branch']->name }} for business {{ $payload['branch']->business->name }} has been approved.
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
