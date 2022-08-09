9722e778f7f5d2f2d13f517a2c52204313e12ef5
LuksfordI <Luksford.Isack@ubx.co.tz>
2022-08-09 08:42:30 +0300

    creating verificaion for vat return and other minor changes

--- app/Http/Controllers/Verification/TaxVerificationApprovalController.php
+++ app/Http/Controllers/Verification/TaxVerificationApprovalController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Verification;
 
 use App\Http\Controllers\Controller;
 use App\Models\Returns\Petroleum\PetroleumReturn;
+use App\Models\Returns\Vat\VatReturn;
 use App\Models\Verification\TaxVerification;
 
 class TaxVerificationApprovalController extends Controller
@@ -24,6 +25,11 @@ class TaxVerificationApprovalController extends Controller
             return view('verification.approval.approval', compact('return', 'verification', 'viewRender'));
         }
 
+        elseif ($return instanceof VatReturn) {
+            $viewRender = "returns.vat_returns.details";
+            return view('verification.approval.approval', compact('return', 'verification', 'viewRender'));
+        }
+
     }
 
     public function show($id){
@@ -32,6 +38,11 @@ class TaxVerificationApprovalController extends Controller
         $return = $verification->taxReturn;
         if($return instanceof PetroleumReturn){
             $viewRender = "returns.petroleum.filing.details";
+            return view('verification.approval.preview', compact('return', 'verification', 'viewRender'));
+        }
+
+        elseif ($return instanceof VatReturn) {
+            $viewRender = "returns.vat_returns.details";
             return view('verification.approval.preview', compact('return', 'verification', 'viewRender'));
         }
     }
--- app/Http/Controllers/Verification/TaxVerificationAssessmentController.php
+++ app/Http/Controllers/Verification/TaxVerificationAssessmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Verification;
 
 use App\Http\Controllers\Controller;
 use App\Models\Returns\Petroleum\PetroleumReturn;
+use App\Models\Returns\Vat\VatReturn;
 use App\Models\Verification\TaxVerification;
 
 class TaxVerificationAssessmentController extends Controller
@@ -21,6 +22,11 @@ class TaxVerificationAssessmentController extends Controller
         $return = $verification->taxReturn;
         if($return instanceof PetroleumReturn){
             $viewRender = "returns.petroleum.filing.details";
+            return view('verification.approval.preview', compact('return', 'verification', 'viewRender'));
+        }
+
+        elseif ($return instanceof VatReturn) {
+            $viewRender = "returns.vat_returns.details";
             return view('verification.approval.preview', compact('return', 'verification', 'viewRender'));
         }
     }
