3be7d06cff44b2d22b81a8b9ae178f313ef5ba4a
Petro-Joseph <petro.joseph@ubx.co.tz>
2022-08-09 22:28:02 +0300

    Update approval.blade.php

--- resources/views/verification/approval/approval.blade.php
+++ resources/views/verification/approval/approval.blade.php
@@ -56,96 +56,96 @@
                         Compliance Officers
                     </div>
                     <div class="card-body">
-                        <<<<<<< HEAD @foreach ($verification->officers as $officer)
+                        @foreach ($verification->officers as $officer)
                             <div class="col-md-6 mb-3">
                                 <span class="font-weight-bold text-uppercase">Team
                                     {{ $officer->team_leader ? 'Leader' : 'Member' }}</span>
                                 <p class="my-1">{{ $officer->user->full_name ?? '' }}</p>
                             </div>
-            @endforeach
-            =======
-            <div class="row">
-                @foreach ($verification->officers as $officer)
-                    <div class="col-md-6 mb-3">
-                        <span class="font-weight-bold text-uppercase">Team
-                            {{ $officer->team_leader ? 'Leader' : 'Member' }}</span>
-                        <p class="my-1">{{ $officer->user->full_name ?? '' }}</p>
-                    </div>
-                @endforeach
-            </div>
-            >>>>>>> DEV
-        </div>
-    </div>
-    @endif
+                        @endforeach
 
-    @if ($verification->assessment)
-        <div class="card">
-            <div class="card-header text-uppercase font-weight-bold bg-white">
-                Assessment Details
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-4 mb-3">
-                        <span class="font-weight-bold text-uppercase">Principal Amount</span>
-                        <p class="my-1">{{ $verification->assessment->principal_amount ?? '' }}</p>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <span class="font-weight-bold text-uppercase">Interest Amount</span>
-                        <p class="my-1">{{ $verification->assessment->interest_amount ?? '' }}</p>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <span class="font-weight-bold text-uppercase">Penalty Amount</span>
-                        <p class="my-1">{{ $verification->assessment->penalty_amount ?? '' }}</p>
-                    </div>
-
-                    @if ($verification->assessment->report_path)
-                        <div class="col-md-4">
-                            <div style="background: #faf5f5; color: #863d3c; border: .5px solid #863d3c24;"
-                                class="p-2 mb-3 d-flex rounded-sm align-items-center">
-                                <i class="bi bi-file-earmark-pdf-fill px-2" style="font-size: x-large"></i>
-                                <a target="_blank"
-                                    href="{{ route('tax_verifications.files.show', encrypt($verification->assessment->report_path)) }}"
-                                    style="font-weight: 500;" class="ml-1">
-                                    Verification Report
-                                    <i class="bi bi-arrow-up-right-square ml-1"></i>
-                                </a>
-                            </div>
+                        <div class="row">
+                            @foreach ($verification->officers as $officer)
+                                <div class="col-md-6 mb-3">
+                                    <span class="font-weight-bold text-uppercase">Team
+                                        {{ $officer->team_leader ? 'Leader' : 'Member' }}</span>
+                                    <p class="my-1">{{ $officer->user->full_name ?? '' }}</p>
+                                </div>
+                            @endforeach
                         </div>
-                    @endif
+
+                    </div>
                 </div>
-            </div>
+            @endif
+
+            @if ($verification->assessment)
+                <div class="card">
+                    <div class="card-header text-uppercase font-weight-bold bg-white">
+                        Assessment Details
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-4 mb-3">
+                                <span class="font-weight-bold text-uppercase">Principal Amount</span>
+                                <p class="my-1">{{ $verification->assessment->principal_amount ?? '' }}</p>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <span class="font-weight-bold text-uppercase">Interest Amount</span>
+                                <p class="my-1">{{ $verification->assessment->interest_amount ?? '' }}</p>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <span class="font-weight-bold text-uppercase">Penalty Amount</span>
+                                <p class="my-1">{{ $verification->assessment->penalty_amount ?? '' }}</p>
+                            </div>
+
+                            @if ($verification->assessment->report_path)
+                                <div class="col-md-4">
+                                    <div style="background: #faf5f5; color: #863d3c; border: .5px solid #863d3c24;"
+                                        class="p-2 mb-3 d-flex rounded-sm align-items-center">
+                                        <i class="bi bi-file-earmark-pdf-fill px-2" style="font-size: x-large"></i>
+                                        <a target="_blank"
+                                            href="{{ route('tax_verifications.files.show', encrypt($verification->assessment->report_path)) }}"
+                                            style="font-weight: 500;" class="ml-1">
+                                            Verification Report
+                                            <i class="bi bi-arrow-up-right-square ml-1"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            @endif
+
+
+            <livewire:approval.tax-verification-approval-processing modelName='{{ get_class($verification) }}'
+                modelId="{{ $verification->id }}" />
         </div>
-    @endif
-
-
-    <livewire:approval.tax-verification-approval-processing modelName='{{ get_class($verification) }}'
-        modelId="{{ $verification->id }}" />
-    </div>
-    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-        @if (view()->exists($viewRender))
-            @php echo view($viewRender, compact('return'))->render() @endphp
-        @else
+        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+            @if (view()->exists($viewRender))
+                @php echo view($viewRender, compact('return'))->render() @endphp
+            @else
+                <div class="card">
+                    <div class="card-body">
+                        <div class="alert alert-danger" role="alert">
+                            <h4 class="alert-heading">Error!</h4>
+                            <p>
+                                Configured page not found kindly check with Administrator
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            @endif
+        </div>
+        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
             <div class="card">
                 <div class="card-body">
-                    <div class="alert alert-danger" role="alert">
-                        <h4 class="alert-heading">Error!</h4>
-                        <p>
-                            Configured page not found kindly check with Administrator
-                        </p>
-                    </div>
+                    <livewire:approval.approval-history-table modelName='{{ get_class($verification) }}'
+                        modelId="{{ $verification->id }}" />
+
                 </div>
             </div>
-        @endif
-    </div>
-    <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-        <div class="card">
-            <div class="card-body">
-                <livewire:approval.approval-history-table modelName='{{ get_class($verification) }}'
-                    modelId="{{ $verification->id }}" />
-
-            </div>
         </div>
-    </div>
     </div>
 
 @endsection
