dce48dcb4a26b10cb07f78c305fbfafc623313c2
mangerere <107620925+mangerere@users.noreply.github.com>
2022-08-10 18:01:14 +0300

    Update index.blade.php

--- resources/views/returns/hotel/index.blade.php
+++ resources/views/returns/hotel/index.blade.php
@@ -8,18 +8,7 @@
         Hotel Returns
     </div>
     <div class="card-body mt-0 p-2">
-        <nav class="nav nav-tabs mt-0 border-top-0">
-            <a href="#tab1" class="nav-item nav-link font-weight-bold active">All Hotel Returns</a>
-            <a href="#tab2" class="nav-item nav-link font-weight-bold">Requests</a>
-        </nav>
-        <div class="tab-content px-2 card pt-3 pb-2">
-            <div id="tab1" class="tab-pane fade active show">
-                <livewire:returns.hotel.hotel-returns-table status='all'></livewire:returns.hotel.hotel-returns-table>
-            </div>
-            <div id="tab2" class="tab-pane fade">
-                <livewire:returns.hotel.hotel-returns-table status='submitted'></livewire:returns.hotel.hotel-returns-table>
-            </div>
-        </div>
+        <livewire:returns.hotel.hotel-returns-table status='all'></livewire:returns.hotel.hotel-returns-table>
     </div>
 </div>
 @endsection
