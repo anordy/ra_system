1b922bd5e6d9071a9f697a97ddb02bd4d67e6628
Petro-Joseph <petro.joseph@ubx.co.tz>
2022-08-11 15:58:14 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -166,7 +166,7 @@ Route::middleware(['auth'])->group(function () {
 
     Route::prefix('pdf')->as('pdf.')->group(function () {
         Route::get('all', [AllPdfController::class, 'index'])->name('all');
-        Route::get('all/{$file}', [AllPdfController::class, 'demandNotice'])->name('demand-notice');
+        Route::get('all/{file}', [AllPdfController::class, 'demandNotice'])->name('demand-notice');
     });
 
     Route::prefix('business')->as('business.')->group(function () {
