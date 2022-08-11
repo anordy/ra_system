26836ca5f38fdec68e1fe55612c5f9b59d765676
Kedmon Maquiz <43686770+kedmonmaquiz@users.noreply.github.com>
2022-08-11 16:10:41 +0300

    mno penalties done

---
+++ app/Models/Returns/ExciseDuty/MnoPenalty.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Returns\ExciseDuty;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class MnoPenalty extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $guarded = [];
+
+    public function return(){
+        return $this->belongsTo(MnoReturn::class);
+    }
+}
