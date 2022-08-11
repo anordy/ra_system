2441df207bfd415621c759b502f1054956f1cead
mangerere <107620925+mangerere@users.noreply.github.com>
2022-08-11 16:37:30 +0300

    Add adjust hotel return

--- routes/web.php
+++ routes/web.php
@@ -254,6 +254,8 @@ Route::middleware(['auth'])->group(function () {
 
         Route::get('/hotel', [HotelReturnController::class, 'index'])->name('hotel.index');
         Route::get('/hotel/view/{return_id}', [HotelReturnController::class, 'show'])->name('hotel.show');
+        Route::get('/hotel/adjust/{return_id}', [HotelReturnController::class, 'adjust'])->name('hotel.adjust');
+
 
         Route::name('excise-duty.')->prefix('excise-duty')->group(function () {
             Route::get('/mno', [MnoReturnController::class, 'index'])->name('mno');
