98746360dae4958f14ef849920bd0eac29e19ecf
gozbert <Gozbert.Stanslaus@ubx.co.tz>
2022-08-12 14:33:43 +0300

    fixing errors

--- resources/views/audit/assessment/action.blade.php
+++ resources/views/audit/assessment/action.blade.php
@@ -1,2 +1,2 @@
-<a href="{{ route('tax_auditing.approvals.show', encrypt($value)) }}" class="btn btn-outline-info btn-sm" data-toggle="tooltip"
+<a href="{{ route('tax_auditing.assessments.show', encrypt($value)) }}" class="btn btn-outline-info btn-sm" data-toggle="tooltip"
     data-placement="right" title="View">View More</a>
