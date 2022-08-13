a5e8af4ca80500838c861cf9ed79e49b95292646
Kedmon Maquiz <43686770+kedmonmaquiz@users.noreply.github.com>
2022-08-13 11:55:44 +0300

    cards for returns summary

--- resources/views/livewire/returns/return-summary.blade.php
+++ resources/views/livewire/returns/return-summary.blade.php
@@ -26,7 +26,7 @@
     <div class="col-md-3">
         <div class="card bg-c-yellow order-card">
             <div class="card-block">
-                <h6 class="m-b-20">Late Filings</h6>
+                <h6 class="m-b-20">Total Late Filings</h6>
                 <h2 class="text-right mb-0"><i class="bi bi-building f-left"></i><span>{{ $totalLateFiledReturns }}</span></h2>
             </div>
         </div>
@@ -34,7 +34,7 @@
     <div class="col-md-3">
         <div class="card bg-c-yellow order-card">
             <div class="card-block">
-                <h6 class="m-b-20">Late Paid</h6>
+                <h6 class="m-b-20">Total Late Paid</h6>
                 <h2 class="text-right mb-0"><i class="bi bi-building f-left"></i><span>{{ $totalLatePaidReturns }}</span></h2>
             </div>
         </div>
