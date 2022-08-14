ec7341738c46a4cdf11c06e1250d98052d8c2fb1
Kedmon Maquiz <43686770+kedmonmaquiz@users.noreply.github.com>
2022-08-15 00:42:32 +0300

    modify mno_returns summary

--- resources/views/livewire/returns/return-summary.blade.php
+++ resources/views/livewire/returns/return-summary.blade.php
@@ -1,41 +1,41 @@
 <div class="row">
-    <div class="col-md-3">
+    <div class="col-md-2">
         <div class="card bg-c-yellow order-card">
-            <div class="card-block">
-                <h6 class="m-b-20">Total Submitted</h6>
-                <h2 class="text-right mb-0"><i class="bi bi-building f-left"></i><span>{{ $totalSubmittedReturns }}</span></h2>
+            <div class="card-block p-2">
+                <span class="m-b-20">Submitted Returns</span>
+                <h4 class="text-right mb-0"><i class="bi bi-building f-left"></i><span>{{ $totalSubmittedReturns }}</span></h4>
             </div>
         </div>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-2">
         <div class="card bg-c-yellow order-card">
-            <div class="card-block">
-                <h6 class="m-b-20">Total Paid</h6>
-                <h2 class="text-right mb-0"><i class="bi bi-building f-left"></i><span>{{ $totalPaidReturns }}</span></h2>
+            <div class="card-block p-2">
+                <span class="m-b-20">Paid Returns</span>
+                <h4 class="text-right mb-0"><i class="bi bi-building f-left"></i><span>{{ $totalPaidReturns }}</span></h4>
             </div>
         </div>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-2">
         <div class="card bg-c-yellow order-card">
-            <div class="card-block">
-                <h6 class="m-b-20">Total unpaid</h6>
-                <h2 class="text-right mb-0"><i class="bi bi-building f-left"></i><span>{{ $totalUnpaidReturns }}</span></h2>
+            <div class="card-block p-2">
+                <span class="m-b-20">unpaid Returns</span>
+                <h4 class="text-right mb-0"><i class="bi bi-building f-left"></i><span>{{ $totalUnpaidReturns }}</span></h4>
             </div>
         </div>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-2">
         <div class="card bg-c-yellow order-card">
-            <div class="card-block">
-                <h6 class="m-b-20">Total Late Filings</h6>
-                <h2 class="text-right mb-0"><i class="bi bi-building f-left"></i><span>{{ $totalLateFiledReturns }}</span></h2>
+            <div class="card-block p-2">
+                <span class="m-b-20">Late Filed Returns</span>
+                <h4 class="text-right mb-0"><i class="bi bi-building f-left"></i><span>{{ $totalLateFiledReturns }}</span></h4>
             </div>
         </div>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-2">
         <div class="card bg-c-yellow order-card">
-            <div class="card-block">
-                <h6 class="m-b-20">Total Late Paid</h6>
-                <h2 class="text-right mb-0"><i class="bi bi-building f-left"></i><span>{{ $totalLatePaidReturns }}</span></h2>
+            <div class="card-block p-2">
+                <span class="m-b-20">Late Paid Returns</span>
+                <h4 class="text-right mb-0"><i class="bi bi-building f-left"></i><span>{{ $totalLatePaidReturns }}</span></h4>
             </div>
         </div>
     </div>
