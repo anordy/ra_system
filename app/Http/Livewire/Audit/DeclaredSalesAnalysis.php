8870421b89b7789aa007d0d33de68260a6f71e50
GeraldMarkNjau <108731600+GeraldMarkNjau@users.noreply.github.com>
2022-08-15 09:42:56 +0300

    Working on displaying audit return details for returns without purchases (Bfo, EmTransaction, MmTransfer, Petroleum)

--- app/Http/Livewire/Audit/DeclaredSalesAnalysis.php
+++ app/Http/Livewire/Audit/DeclaredSalesAnalysis.php
@@ -3,12 +3,17 @@
 namespace App\Http\Livewire\Audit;
 
 use App\Models\BusinessLocation;
+use App\Models\FinancialMonth;
 use App\Models\Returns\BFO\BfoConfig;
 use App\Models\Returns\BFO\BfoReturn;
 use App\Models\Returns\BFO\BfoReturnItems;
+use App\Models\Returns\EmTransactionConfig;
+use App\Models\Returns\EmTransactionReturnItem;
 use App\Models\Returns\ExciseDuty\MnoConfig;
 use App\Models\Returns\HotelReturns\HotelReturnConfig;
 use App\Models\Returns\HotelReturns\HotelReturnItem;
+use App\Models\Returns\MmTransferConfig;
+use App\Models\Returns\MmTransferReturnItem;
 use App\Models\Returns\Petroleum\PetroleumConfig;
 use App\Models\Returns\Petroleum\PetroleumReturnItem;
 use App\Models\Returns\Vat\VatReturnConfig;
@@ -18,6 +23,7 @@ use Carbon\Carbon;
 use DateTime;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
+use stdClass;
 
 class DeclaredSalesAnalysis extends Component
 {
@@ -33,6 +39,12 @@ class DeclaredSalesAnalysis extends Component
     public $returns = [];
     public $taxType;
     public $branch;
+    public $headersBfo;
+    public $headersEmTransaction;
+    public $headersMmTransfer;
+    public $headersPetroleum;
+    public $withoutPurchases = false;
+    public $returnTypeTable;
 
     public function mount($audit)
     {
@@ -48,6 +60,7 @@ class DeclaredSalesAnalysis extends Component
                 $this->hotel();
                 break;
             case TaxType::PETROLEUM:
+                $this->returnTypeTable = TaxType::PETROLEUM;
                 $this->petroleum();
                 break;
             case TaxType::VAT:
@@ -57,7 +70,16 @@ class DeclaredSalesAnalysis extends Component
                 $this->mno();
                 break;
             case TaxType::EXCISE_DUTY_BFO:
+                $this->returnTypeTable = TaxType::EXCISE_DUTY_BFO;
                 $this->bfo();
+                break;
+            case TaxType::ELECTRONIC_MONEY_TRANSACTION:
+                $this->returnTypeTable = TaxType::ELECTRONIC_MONEY_TRANSACTION;
+                $this->emTransaction();
+                break;
+            case TaxType::MOBILE_MONEY_TRANSFER:
+                $this->returnTypeTable = TaxType::MOBILE_MONEY_TRANSFER;
+                $this->mmTransfer();
                 break;
         }
 
@@ -120,49 +142,22 @@ class DeclaredSalesAnalysis extends Component
 
     protected function petroleum()
     {
-        $purchaseConfigs = PetroleumConfig::whereIn('code', ["LP", "IP"])->get()->pluck('id');
-
-        $this->purchases = PetroleumReturnItem::selectRaw('financial_months.name as month, financial_years.code as year, SUM(value) as total_purchases, SUM(vat) as total_purchases_vat')
-            ->leftJoin('hotel_return_configs', 'hotel_return_configs.id', 'hotel_return_items.config_id')
-            ->leftJoin('hotel_returns', 'hotel_returns.id', 'hotel_return_items.return_id')
-            ->leftJoin('financial_months', 'financial_months.id', 'hotel_returns.financial_month_id')
+        $salesConfigs = PetroleumConfig::where('code', '!=', 'TOTAL')->get()->pluck('id');
+        $headers = PetroleumConfig::where('code', '!=', 'TOTAL')->get()->pluck('name');
+        
+        $yearReturnGroup = PetroleumReturnItem::select('petroleum_configs.code', 'petroleum_return_items.value', 'petroleum_return_items.vat', 'financial_months.name as month', 'financial_years.name as year')
+            ->leftJoin('petroleum_configs', 'petroleum_configs.id', 'petroleum_return_items.config_id')
+            ->leftJoin('petroleum_returns', 'petroleum_returns.id', 'petroleum_return_items.return_id')
+            ->leftJoin('financial_months', 'financial_months.id', 'petroleum_returns.financial_month_id')
             ->leftJoin('financial_years', 'financial_years.id', 'financial_months.financial_year_id')
-            ->where('hotel_returns.tax_type_id', $this->taxType->id)
-            ->where('hotel_returns.business_location_id', $this->branch->id)
-            ->whereIn('config_id', $purchaseConfigs)
-            ->groupBy(['financial_years.code', 'financial_months.name'])->get();
-
-
-        $salesConfigs = PetroleumConfig::whereIn('code', ["HS", "RS", "TOS", "OS"])->get()->pluck('id');
-
-        $this->sales = PetroleumReturnItem::selectRaw('financial_months.name as month, financial_years.code as year, SUM(value) as total_sales, SUM(vat) as total_sales_vat')
-            ->leftJoin('hotel_return_configs', 'hotel_return_configs.id', 'hotel_return_items.config_id')
-            ->leftJoin('hotel_returns', 'hotel_returns.id', 'hotel_return_items.return_id')
-            ->leftJoin('financial_months', 'financial_months.id', 'hotel_returns.financial_month_id')
-            ->leftJoin('financial_years', 'financial_years.id', 'financial_months.financial_year_id')
-            ->where('hotel_returns.tax_type_id', $this->taxType->id)
-            ->where('hotel_returns.business_location_id', $this->branch->id)
             ->whereIn('config_id', $salesConfigs)
-            ->groupBy(['financial_years.code', 'financial_months.name'])->get();
+            ->get()->groupBy(['year','month']);
+        
+        $yearData = $this->formatDataArray($yearReturnGroup);
 
-        $returns = array_replace_recursive($this->purchases->toArray(), $this->sales->toArray());
-
-        $calculations = collect(array_map(function ($returns) {
-            return array(
-                'year' => $returns['year'],
-                'month' => $returns['month'],
-                'financial_month' => "{$returns['month']} {$returns['year']}",
-                'total_sales' => $returns['total_sales'],
-                'total_purchases' => $returns['total_purchases'],
-                'output_vat' => $returns['total_sales_vat'],
-                'input_tax' => $returns['total_purchases_vat'],
-                'tax_paid' => ($returns['total_sales_vat']) - $returns['total_purchases_vat'],
-            );
-        }, $returns));
-
-        dd($calculations, 'here');
-
-        $this->returns = $calculations ?? [];
+        $this->withoutPurchases = true;
+        $this->returns = $yearData;
+        $this->headersPetroleum = $headers;
     }
 
     public function vat()
@@ -218,37 +213,95 @@ class DeclaredSalesAnalysis extends Component
 
     protected function bfo()
     {
-        $salesConfigs = BfoConfig::whereIn('code', ["HS", "RS", "TOS", "OS"])->get()->pluck('id');
+        $salesConfigs = BfoConfig::where('code', '!=', 'TotalFBO')->get()->pluck('id');
+        $headers = BfoConfig::where('code', '!=', 'TotalFBO')->get()->pluck('name');
 
-        $this->sales = BfoReturnItems::selectRaw('financial_months.name as month, financial_years.code as year, SUM(value) as total_sales, SUM(vat) as total_sales_vat')
-            ->leftJoin('bfo_configs', 'bfo_configs.id', 'hotel_return_items.config_id')
-            ->leftJoin('bfo_returns', 'bfo_returns.id', 'hotel_return_items.return_id')
+        $yearReturnGroup = BfoReturnItems::select('bfo_configs.code', 'bfo_return_items.value', 'bfo_return_items.vat', 'financial_months.name as month', 'financial_years.name as year')
+            ->leftJoin('bfo_configs', 'bfo_configs.id', 'bfo_return_items.config_id')
+            ->leftJoin('bfo_returns', 'bfo_returns.id', 'bfo_return_items.bfo_return_id')
             ->leftJoin('financial_months', 'financial_months.id', 'bfo_returns.financial_month_id')
             ->leftJoin('financial_years', 'financial_years.id', 'financial_months.financial_year_id')
-            ->where('bfo_returns.tax_type_id', $this->taxType->id)
-            ->where('bfo_returns.business_location_id', $this->branch->id)
             ->whereIn('config_id', $salesConfigs)
-            ->groupBy(['financial_years.code', 'financial_months.name'])->get();
-            
-        dd($this->sales);
-        $returns = array_replace_recursive($this->purchases->toArray(), $this->sales->toArray());
+            ->get()->groupBy(['year','month']);
+        
 
-        $calculations = collect(array_map(function ($returns) {
-            return array(
-                'year' => $returns['year'],
-                'month' => $returns['month'],
-                'financial_month' => "{$returns['month']} {$returns['year']}",
-                'total_sales' => $returns['total_sales'],
-                'total_purchases' => $returns['total_purchases'],
-                'output_vat' => $returns['total_sales_vat'],
-                'input_tax' => $returns['total_purchases_vat'],
-                'tax_paid' => ($returns['total_sales_vat']) - $returns['total_purchases_vat'],
-            );
-        }, $returns));
+        $yearData = $this->formatDataArray($yearReturnGroup);
 
-        dd($calculations, 'here');
+        $this->withoutPurchases = true;
+        $this->returns = $yearData;
+        $this->headersBfo = $headers;
 
-        $this->returns = $calculations ?? [];
+    }
+
+    protected function emTransaction()
+    {
+        $salesConfigs = EmTransactionConfig::where('code', '!=', 'TotalEMT')->get()->pluck('id');
+        $headers = EmTransactionConfig::where('code', '!=', 'TotalEMT')->get()->pluck('name');
+
+        $yearReturnGroup = EmTransactionReturnItem::select('em_transaction_configs.code', 'em_transaction_return_items.value', 'em_transaction_return_items.vat', 'seven_days_financial_months.name as month', 'financial_years.name as year')
+            ->leftJoin('em_transaction_configs', 'em_transaction_configs.id', 'em_transaction_return_items.config_id')
+            ->leftJoin('em_transaction_returns', 'em_transaction_returns.id', 'em_transaction_return_items.return_id')
+            ->leftJoin('seven_days_financial_months', 'seven_days_financial_months.id', 'em_transaction_returns.financial_month_id')
+            ->leftJoin('financial_years', 'financial_years.id', 'seven_days_financial_months.financial_year_id')
+            ->whereIn('config_id', $salesConfigs)
+            ->get()->groupBy(['year','month']);
+        
+
+        $yearData = $this->formatDataArray($yearReturnGroup);
+
+        $this->withoutPurchases = true;
+        $this->returns = $yearData;
+        $this->headersEmTransaction = $headers;
+
+    }
+
+    protected function mmTranfer()
+    {
+        $salesConfigs = MmTransferConfig::where('code', '!=', 'TotalEMT')->get()->pluck('id');
+        $headers = MmTransferConfig::where('code', '!=', 'TotalEMT')->get()->pluck('name');
+
+        $yearReturnGroup = MmTransferReturnItem::select('mm_transfer_configs.code', 'mm_transfer_return_items.value', 'mm_transfer_return_items.vat', 'seven_days_financial_months.name as month', 'financial_years.name as year')
+            ->leftJoin('mm_transfer_configs', 'mm_transfer_configs.id', 'mm_transfer_return_items.config_id')
+            ->leftJoin('mm_transfer_returns', 'mm_transfer_returns.id', 'mm_transfer_return_items.return_id')
+            ->leftJoin('seven_days_financial_months', 'seven_days_financial_months.id', 'mm_transfer_returns.financial_month_id')
+            ->leftJoin('financial_years', 'financial_years.id', 'seven_days_financial_months.financial_year_id')
+            ->whereIn('config_id', $salesConfigs)
+            ->get()->groupBy(['year','month']);
+
+        $yearData = $this->formatDataArray($yearReturnGroup);
+        
+
+        $this->withoutPurchases = true;
+        $this->returns = $yearData;
+        $this->headersMmTransfer = $headers;
+
+    }
+
+    protected function formatDataArray($yearReturnGroup){
+        $yearData = [];
+
+
+        foreach($yearReturnGroup as $keyYear => $monthreturnGroup){
+            $monthData = [];
+            foreach($monthreturnGroup as $keyMonth => $returnItems){
+                $itemValue = [
+                    'month' => $keyMonth,
+                ];
+                $totalVat = 0;
+                $totalValue = 0;
+                foreach ($returnItems as $keyItem => $item) {
+                    $itemValue[$item['code']]=$item['value'];
+                    $totalValue += $item['value'];
+                    $totalVat += $item['vat'];
+                }
+                $itemValue['totalValue'] = $totalValue;
+                $itemValue['totalVat'] = $totalVat;
+                $monthData[] =  $itemValue;
+            }
+            $yearData[$keyYear] = $monthData;
+        }
+
+        return $yearData;
     }
 
     public function render()
---
+++ resources/views/investigation/approval/return-details/emtransaction.blade.php
@@ -0,0 +1,58 @@
+<div class="card">
+    <div class="card-body">
+        <h6 class="text-uppercase mt-2 ml-2">Declared Sales Analysis</h6>
+        <hr>
+        <div class="row mx-2">
+            @foreach ($returns as $year => $return)
+                <strong>{{ $year }}</strong>
+                @php
+                    $LMTRANSFER = 0;
+                    $LWITHDRAWALS = 0;
+                    $totalValue = 0;
+                    $totalVat = 0;
+                @endphp
+                <table class="table table-sm table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Month</th>
+                            @foreach ($headersEmTransaction as $header)
+                                <th>{{ $header }}</th>
+                            @endforeach
+                            <th>Total</th>
+                            <th>Output VAT</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        @foreach ($return as $item)
+                            <tr>
+                                <td>{{ $item['month'] }}</td>
+                                <td>{{ $item['LMTRANSFER'] }}</td>
+                                <td>{{ $item['LWITHDRAWALS'] }}</td>
+                                <td>{{ $item['totalValue'] }}</td>
+                                <td>{{ $item['totalVat'] }}</td>
+                            </tr>
+                            @php
+                                $LMTRANSFER += $item['LMTRANSFER'];
+                                $LWITHDRAWALS += $item['LWITHDRAWALS'];
+                                $totalValue += $item['totalValue'];
+                                $totalVat += $item['totalVat'];
+                            @endphp
+                        @endforeach
+                    </tbody>
+                    <tfoot>
+                        <tr class="font-weight-bold">
+                            <td>TOTAL</td>
+                            <td>{{ number_format($LMTRANSFER, 2) }}</td>
+                            <td>{{ number_format($LWITHDRAWALS, 2) }}</td>
+                            <td>{{ number_format($totalValue, 2) }}</td>
+                            <td>{{ number_format($totalVat, 2) }}</td>
+                        </tr>
+                    </tfoot>
+
+                </table>
+            @endforeach
+
+        </div>
+    </div>
+</div>
