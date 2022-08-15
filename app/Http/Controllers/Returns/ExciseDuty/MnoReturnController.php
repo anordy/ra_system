4e75e4e72df85a2cefd7f96af27ee33b0c6476f2
Kedmon Maquiz <43686770+kedmonmaquiz@users.noreply.github.com>
2022-08-15 05:47:27 +0300

    modify returns summaries

--- app/Http/Controllers/Returns/ExciseDuty/MnoReturnController.php
+++ app/Http/Controllers/Returns/ExciseDuty/MnoReturnController.php
@@ -17,7 +17,8 @@ class MnoReturnController extends Controller
 {
     use ReturnCardReport;
 
-    public function index(){
+    public function index()
+    {
 
         $data = $this->returnCardReport(MnoReturn::class, 'mno', 'mno');
 
@@ -29,49 +30,50 @@ class MnoReturnController extends Controller
 
         //total submitted returns
         $vars['totalSubmittedReturns'] = DB::table('mno_returns')
-                    ->join('financial_months','financial_months.id','mno_returns.financial_month_id')
-                    ->whereBetween('financial_months.due_date',[$from,$to])
-                    ->count();
-        
+            ->join('financial_months', 'financial_months.id', 'mno_returns.financial_month_id')
+            ->whereBetween('financial_months.due_date', [$from, $to])
+            ->count();
+
         //total paid returns
         $vars['totalPaidReturns'] = DB::table('mno_returns')
-                    ->join('financial_months','financial_months.id','mno_returns.financial_month_id')
-                    ->where('mno_returns.status','complete')
-                    ->whereBetween('financial_months.due_date',[$from,$to])
-                    ->count();
+            ->join('financial_months', 'financial_months.id', 'mno_returns.financial_month_id')
+            ->where('mno_returns.status', 'complete')
+            ->whereBetween('financial_months.due_date', [$from, $to])
+            ->count();
 
         //total unpaid returns
         $vars['totalUnpaidReturns'] = DB::table('mno_returns')
-                    ->join('businesses','businesses.id','mno_returns.business_id') 
-                    ->join('financial_months','financial_months.id','mno_returns.financial_month_id')
-                    ->where('businesses.status',BusinessStatus::APPROVED)
-                    ->where('mno_returns.status','!=','complete')
-                    ->whereBetween('financial_months.due_date',[$from,$to])
-                    ->count();
+            ->join('businesses', 'businesses.id', 'mno_returns.business_id')
+            ->join('financial_months', 'financial_months.id', 'mno_returns.financial_month_id')
+            ->where('businesses.status', BusinessStatus::APPROVED)
+            ->where('mno_returns.status', '!=', 'complete')
+            ->whereBetween('financial_months.due_date', [$from, $to])
+            ->count();
 
         //late filed returns
         $vars['totalLateFiledReturns'] = DB::table('mno_returns')
-                    ->join('financial_months', 'mno_returns.financial_month_id','financial_months.id')
-                    ->where('mno_returns.created_at','>','financial_months.due_date')
-                    ->whereBetween('financial_months.due_date',[$from,$to])
-                    ->count();
-        
+            ->join('financial_months', 'mno_returns.financial_month_id', 'financial_months.id')
+            ->where('mno_returns.created_at', '>', 'financial_months.due_date')
+            ->whereBetween('financial_months.due_date', [$from, $to])
+            ->count();
+
         //total late paid returns
         $vars['totalLatePaidReturns'] = DB::table('mno_returns')
-                    ->join('financial_months', 'mno_returns.financial_month_id','financial_months.id')
-                    ->join('zm_bills','mno_returns.id','zm_bills.billable_id')
-                    ->join('zm_payments','zm_payments.zm_bill_id','zm_bills.id')
-                    ->whereBetween('financial_months.due_date',[$from,$to])
-                    ->where('zm_bills.billable_type',MnoReturn::class)
-                    ->where('mno_returns.status','complete')
-                    ->where('mno_returns.created_at','>','zm_payments.trx_time')
-                    ->count();
+            ->join('financial_months', 'mno_returns.financial_month_id', 'financial_months.id')
+            ->join('zm_bills', 'mno_returns.id', 'zm_bills.billable_id')
+            ->join('zm_payments', 'zm_payments.zm_bill_id', 'zm_bills.id')
+            ->whereBetween('financial_months.due_date', [$from, $to])
+            ->where('zm_bills.billable_type', MnoReturn::class)
+            ->where('mno_returns.status', 'complete')
+            ->where('mno_returns.created_at', '>', 'zm_payments.trx_time')
+            ->count();
 
-        return view('returns.excise-duty.mno.index',compact('vars','data'));
+        return view('returns.excise-duty.mno.index', compact('vars', 'data'));
     }
 
-    public function show($id){
+    public function show($id)
+    {
         $return = MnoReturn::find(decrypt($id));
-        return view('returns.excise-duty.mno.show',compact('return'));
+        return view('returns.excise-duty.mno.show', compact('return'));
     }
 }
--- app/Http/Controllers/Returns/LumpSum/LumpSumReturnController.php
+++ app/Http/Controllers/Returns/LumpSum/LumpSumReturnController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Returns\LumpSum;
 use App\Http\Controllers\Controller;
 use App\Http\Livewire\Returns\LumpSum\LumpSumReturns;
 use App\Models\BusinessLocation;
+use App\Models\BusinessStatus;
 use App\Models\Returns\LumpSum\LumpSumReturn;
 use App\Traits\ReturnCardReport;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -16,31 +18,54 @@ class LumpSumReturnController extends Controller
 
     public function index()
     {
-        
+
         $data = $this->returnCardReport(LumpSumReturn::class, 'lump_sum', 'lump_sum');
 
-        $vars['totalSubmittedReturns'] = LumpSumReturn::query()->whereNotNull('created_at')->count();
+        //last day of last month
+        $from = Carbon::now()->subMonth()->lastOfMonth()->toDateTimeString();
+
+        //fist day of next month
+        $to = Carbon::now()->addMonth()->firstOfMonth()->toDateTimeString();
+
+        //total submitted returns
+        $vars['totalSubmittedReturns'] = DB::table('lump_sum_returns')
+            ->join('financial_months', 'financial_months.id', 'lump_sum_returns.financial_month_id')
+            ->whereBetween('financial_months.due_date', [$from, $to])
+            ->count();
 
         //total paid returns
-        $vars['totalPaidReturns'] = LumpSumReturn::where('status', 'complete')->count();
+        $vars['totalPaidReturns'] = DB::table('lump_sum_returns')
+            ->join('financial_months', 'financial_months.id', 'lump_sum_returns.financial_month_id')
+            ->where('lump_sum_returns.status', 'complete')
+            ->whereBetween('financial_months.due_date', [$from, $to])
+            ->count();
 
         //total unpaid returns
-        $vars['totalUnpaidReturns'] = LumpSumReturn::where('status', '!=', 'complete')->count();
+        $vars['totalUnpaidReturns'] = DB::table('lump_sum_returns')
+            ->join('businesses', 'businesses.id', 'lump_sum_returns.business_id')
+            ->join('financial_months', 'financial_months.id', 'lump_sum_returns.financial_month_id')
+            ->where('businesses.status', BusinessStatus::APPROVED)
+            ->where('lump_sum_returns.status', '!=', 'complete')
+            ->whereBetween('financial_months.due_date', [$from, $to])
+            ->count();
 
         //late filed returns
         $vars['totalLateFiledReturns'] = DB::table('lump_sum_returns')
-                    ->join('financial_months', 'lump_sum_returns.financial_month_id', 'financial_months.id')
-                    ->where('lump_sum_returns.created_at', '>', 'financial_months.due_date')
-                    ->count();
-        
+            ->join('financial_months', 'lump_sum_returns.financial_month_id', 'financial_months.id')
+            ->where('lump_sum_returns.created_at', '>', 'financial_months.due_date')
+            ->whereBetween('financial_months.due_date', [$from, $to])
+            ->count();
+
         //total late paid returns
         $vars['totalLatePaidReturns'] = DB::table('lump_sum_returns')
-                    ->join('zm_bills', 'lump_sum_returns.id', 'zm_bills.billable_id')
-                    ->join('zm_payments', 'zm_payments.zm_bill_id', 'zm_bills.id')
-                    ->where('zm_bills.billable_type', LumpSumReturn::class)
-                    ->where('lump_sum_returns.status', 'complete')
-                    ->where('lump_sum_returns.created_at', '>', 'zm_payments.trx_time')
-                    ->count();
+            ->join('financial_months', 'lump_sum_returns.financial_month_id', 'financial_months.id')
+            ->join('zm_bills', 'lump_sum_returns.id', 'zm_bills.billable_id')
+            ->join('zm_payments', 'zm_payments.zm_bill_id', 'zm_bills.id')
+            ->whereBetween('financial_months.due_date', [$from, $to])
+            ->where('zm_bills.billable_type', LumpSumReturn::class)
+            ->where('lump_sum_returns.status', 'complete')
+            ->where('lump_sum_returns.created_at', '>', 'zm_payments.trx_time')
+            ->count();
 
         return view('returns.lumpsum.history', compact('vars', 'data'));
     }
@@ -55,7 +80,7 @@ class LumpSumReturnController extends Controller
 
         return view('returns.lump-sum.lump-sum', compact('location', 'tax_type', 'business', 'filling_month_id'));
     }
-    
+
     public function history()
     {
         return view('returns.lumpsum.history');
@@ -65,7 +90,7 @@ class LumpSumReturnController extends Controller
     {
         $row = decrypt($row);
         $id  = $row->id;
-        
+
         $return = LumpSumReturn::findOrFail($id);
 
         return view('returns.lumpsum.view', compact('return'));
