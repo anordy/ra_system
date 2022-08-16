ab5f34f3cadfde5cf9889fe1b7e8b764e6fe16d4
Meshack Fungo <meshack.fungo@ubx.co.tz>
2022-08-16 13:13:51 +0300

    Initial return payment extension and installment payments

--- resources/views/returns/stamp-duty/show.blade.php
+++ resources/views/returns/stamp-duty/show.blade.php
@@ -9,10 +9,8 @@
         </div>
     </div>
 
-    <x-bill-structure :bill="$return->bill" />
-
-    <div class="card">
-        <div class="card-header bg-white">
+    <div class="card rounded-0">
+        <div class="card-header bg-white font-weight-bold">
             Returns Details
         </div>
         <div class="card-body">
@@ -42,56 +40,54 @@
                     <p class="my-1">{{ $return->branch->name ?? 'Head Quarter' }}</p>
                 </div>
             </div>
-
-
-
-            <div class="row mt-3">
-                <div class="col-md-12">
-                    <p class="text-uppercase font-weight-bold">Return Items</p>
-                </div>
-                <div class="col-md-12">
-                    <table class="table table-bordered table-striped normal-text">
-                        <thead>
-                        <th style="width: 30%">Item Name</th>
-                        <th style="width: 20%">Value(TZS)</th>
-                        <th style="width: 10%">Rate</th>
-                        <th style="width: 20%">Tax(TZS)</th>
-                        </thead>
-                        <tbody>
-                        @foreach ($return->items as $item)
-                            <tr>
-                                <td>{{ $item->config->name }}</td>
-                                <td>{{ number_format($item->value) }}</td>
-                                <td>{{ $item->config->rate_type === 'percentage' ? $item->config->rate : $item->config->rate_usd }}</td>
-                                <td>{{ number_format($item->tax) }}</td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                        <tfoot>
-                        <tr>
-                            <th style="width: 20%"></th>
-                            <th style="width: 30%"></th>
-                            <th style="width: 25%"></th>
-                            <th style="width: 25%">{{ number_format($return->total_amount_due) }}</th>
-                        </tr>
-
-                        </tfoot>
-                    </table>
-                </div>
-            </div>
-
-            <div class="row mt-3">
-                <div class="col-md-12">
-                    <p class="text-uppercase font-weight-bold">Payment Summary</p>
-                </div>
-                <livewire:returns.returns-penalty modelName="App\Models\Returns\StampDuty\StampDutyReturn" modelId="{{ $return->id }}" />
-            </div>
-
-            <div class="row">
-                <div class="col-md-12 d-flex justify-content-end">
-
-                </div>
-            </div>
         </div>
     </div>
+    <div class="card rounded-0">
+        <div class="card-header bg-white font-weight-bold">
+            Return Structure
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered mb-0 table-sm table-hover">
+                <thead>
+                <th style="width: 30%">Item Name</th>
+                <th style="width: 20%">Value(TZS)</th>
+                <th style="width: 10%">Rate</th>
+                <th style="width: 20%">Tax(TZS)</th>
+                </thead>
+                <tbody>
+                @foreach ($return->items as $item)
+                    <tr>
+                        <td>{{ $item->config->name }}</td>
+                        <td>{{ number_format($item->value) }}</td>
+                        <td>{{ $item->config->rate_type === 'percentage' ? $item->config->rate : $item->config->rate_usd }}</td>
+                        <td>{{ number_format($item->tax) }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+                <tfoot>
+                <tr class="bg-secondary">
+                    <th style="width: 20%">Total</th>
+                    <th style="width: 30%"></th>
+                    <th style="width: 25%"></th>
+                    <th style="width: 25%">{{ number_format($return->total_amount_due) }}</th>
+                </tr>
+
+                </tfoot>
+            </table>
+        </div>
+    </div>
+
+    @if($return->penalty)
+        <div class="card rounded-0">
+            <div class="card-header bg-white font-weight-bold">
+                Return Penalties
+            </div>
+            <div class="card-body">
+                <livewire:returns.returns-penalty modelName="App\Models\Returns\StampDuty\StampDutyReturn" modelId="{{ $return->id }}" />
+            </div>
+        </div>
+    @endif
+
+    <x-bill-structure :bill="$return->bill" />
+
 @endsection
