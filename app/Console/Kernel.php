9818997d7d5a92ad3803862826858ac15d09341c
gsubx <Gozbert.Stanslaus@ubx.co.tz>
2022-08-17 09:52:44 +0300

    fixing data

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,9 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
-        $schedule->command('erate:daily')
-            ->dailyAt('00:00');
+        $schedule->command('daily:penalty')->everyMinute();
     }
 
     /**
