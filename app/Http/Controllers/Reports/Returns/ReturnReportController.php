5c192f8eb282cbc3b6b569fad09631567bf8af6b
Kedmon Maquiz <43686770+kedmonmaquiz@users.noreply.github.com>
2022-08-17 18:31:48 +0300

    fix conflicts

--- app/Http/Controllers/Reports/Returns/ReturnReportController.php
+++ app/Http/Controllers/Reports/Returns/ReturnReportController.php
@@ -11,34 +11,35 @@ class ReturnReportController extends Controller
 {
     use ReturnReportTrait;
 
-    public function index(){
+    public function index()
+    {
         return view('reports.returns.index');
     }
 
     public function preview($parameters)
     {
-        $parameters = json_decode(decrypt($parameters),true);
+        $parameters = json_decode(decrypt($parameters), true);
         // dd($parameters);
-        return view('reports.returns.preview',compact('parameters'));       
+        return view('reports.returns.preview', compact('parameters'));
     }
 
     public function exportReturnReportPdf($parameters)
     {
-        $parameters = json_decode(decrypt($parameters),true);
+        $parameters = json_decode(decrypt($parameters), true);
         // dd($parameters);
         $modelData = $this->getModelData($parameters);
         $records = $this->getRecords($modelData['model'], $parameters);
         $for = $parameters['type'] == 'Filing' ? $parameters['filing_report_type'] : $parameters['payment_report_type'];
         $for = str_replace('-', ' ', $for);
         // dd($records);
-        if($parameters['year']=='all'){
-            $fileName = 'Return Records ('.$for.').pdf';
-            $title = $modelData['returnName'].' Return Records ('.$for.')';
-        }else{
-            $fileName = 'Return Records ('.$for.') - '.$parameters['year'].'.pdf';
-            $title = $modelData['returnName'].' Return Records ('.$for.')';
-        } 
-        $records = $records->get(); 
+        if ($parameters['year'] == 'all') {
+            $fileName = 'Return Records (' . $for . ').pdf';
+            $title = $modelData['returnName'] . ' Return Records (' . $for . ')';
+        } else {
+            $fileName = 'Return Records (' . $for . ') - ' . $parameters['year'] . '.pdf';
+            $title = $modelData['returnName'] . ' Return Records (' . $for . ')';
+        }
+        $records = $records->get();
         $pdf = PDF::loadView('exports.returns.reports.pdf.stamp-duty', compact('records', 'title', 'parameters'));
         $pdf->setPaper('a4', 'portrait');
         $pdf->setOption(['dpi' => 150, 'defaultFont' => 'sans-serif']);
