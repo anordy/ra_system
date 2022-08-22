460c11144c5dd9dc596e38e1a67646c74d899641
Meshack Fungo <meshack.fungo@ubx.co.tz>
2022-08-22 12:29:02 +0300

    Installment improvments

--- app/Http/Livewire/Approval/InstallmentRequestApprovalProcessing.php
+++ app/Http/Livewire/Approval/InstallmentRequestApprovalProcessing.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Livewire\Approval;
 
-use App\Enum\ExtensionStatus;
+use App\Enum\DebtPaymentMethod;
+use App\Enum\ExtensionRequestStatus;
+use App\Enum\InstallmentRequestStatus;
+use App\Enum\InstallmentStatus;
+use App\Models\Debts\Debt;
+use App\Models\Installment\Installment;
 use Carbon\Carbon;
 use Exception;
 use App\Models\TaxType;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 use Illuminate\Support\Facades\Log;
@@ -48,27 +54,55 @@ class InstallmentRequestApprovalProcessing extends Component
             $this->validate([
                 'installmentPhases' => ['required', 'numeric', 'min:1', 'max:12'],
             ]);
-
-            $this->subject->installment_from = Carbon::now()->toDateTimeString();
-            $this->subject->installment_to = Carbon::now()->addMonths($this->installmentPhases);
-            $this->subject->installment_count = $this->installmentPhases;
-            $this->subject->save();
-        }
-
-        if ($this->checkTransition('accepted')) {
-            $this->subject->status = ExtensionStatus::APPROVED;
-            $this->subject->save();
         }
 
         try {
-            $this->doTransition($transtion, ['status' => 'agree', 'comment' => $this->comments, 'operators' => []]);
+            DB::beginTransaction();
+
+            if ($this->checkTransition('debt_manager')) {
+                $this->subject->installment_from = Carbon::now()->toDateTimeString();
+                $this->subject->installment_to = Carbon::now()->addMonths($this->installmentPhases);
+                $this->subject->installment_count = $this->installmentPhases;
+                $this->subject->save();
+            }
+
+            if ($this->checkTransition('accepted')) {
+                $this->subject->status = InstallmentRequestStatus::APPROVED;
+                $debt = Debt::findOrFail($this->subject->debt_id);
+                $debt->update([
+                    'curr_due_date' => $this->subject->installment_to,
+                    'payment_method' => DebtPaymentMethod::INSTALLMENT
+                    // TODO: Maybe update debt to indicate installment ? 🤷
+                ]);
+
+                // Create installment record
+                Installment::create([
+                    'debt_id' => $this->subject->debt_id,
+                    'location_id' => $this->subject->location_id,
+                    'business_id' => $this->subject->business_id,
+                    'tax_type_id' => $this->subject->tax_type_id,
+                    'installment_request_id' => $this->subject->id,
+                    'installment_from' => $this->subject->installment_from,
+                    'installment_to' => $this->subject->installment_to,
+                    'installment_count' => $this->subject->installment_count,
+                    'amount' => $debt->outstanding_amount,
+                    'currency' => $debt->currency,
+                    'status' => InstallmentStatus::ACTIVE
+                ]);
+
+                $this->subject->save();
+            }
+
+            $this->doTransition($transtion, ['status' => 'agree', 'comment' => $this->comments]);
+            DB::commit();
+            $this->flash('success', 'Approved successfully', [], redirect()->back()->getTargetUrl());
         } catch (Exception $e) {
             Log::error($e);
+            DB::rollBack();
             $this->alert('error', $e->getMessage());
             return;
         }
 
-        $this->flash('success', 'Approved successfully', [], redirect()->back()->getTargetUrl());
     }
 
     public function reject($transtion)
@@ -77,15 +111,20 @@ class InstallmentRequestApprovalProcessing extends Component
             'comments' => 'required|string',
         ]);
 
-        if ($this->checkTransition('rejected')) {
-            $this->subject->status = ExtensionStatus::REJECTED;
-            $this->subject->save();
-        }
-
         try {
+            DB::beginTransaction();
+
+            if ($this->checkTransition('rejected')) {
+                $this->subject->status = ExtensionRequestStatus::REJECTED;
+                $this->subject->save();
+            }
+
             $this->doTransition($transtion, ['status' => 'reject', 'comment' => $this->comments]);
+            DB::commit();
         } catch (Exception $e) {
             Log::error($e);
+            DB::rollBack();
+            $this->alert('error', 'Something went wrong, please try again later.');
             return;
         }
         $this->flash('success', 'Rejected successfully', [], redirect()->back()->getTargetUrl());
