92f238a8f917cf77a6afe263357ae9ebfbbcfc73
David Mabula <104975559+davidmbl-UBX@users.noreply.github.com>
2022-08-26 09:19:07 +0300

    land lease added business land leases

--- database/migrations/2022_07_31_151520_create_land_leases_table.php
+++ database/migrations/2022_07_31_151520_create_land_leases_table.php
@@ -30,7 +30,7 @@ class CreateLandLeasesTable extends Migration
             $table->unsignedBigInteger('created_by');
             $table->unsignedBigInteger('edited_by')->nullable();
             $table->enum('category', ['sole owner','business']);
-            $table->unsignedBigInteger('business_location_id');
+            $table->unsignedBigInteger('business_location_id')->nullable();
             $table->string('name')->nullable();
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
