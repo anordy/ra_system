660c8af063e39ab229b511dcbcd46a3ad620b4f6
luksford <107618675+luksford@users.noreply.github.com>
2022-08-26 17:57:05 +0300

    minor changes

--- app/Http/Controllers/Returns/Queries/SalesPurchasesController.php
+++ app/Http/Controllers/Returns/Queries/SalesPurchasesController.php
@@ -7,22 +7,19 @@ use App\Models\Returns\HotelReturns\HotelReturn;
 use App\Models\Returns\HotelReturns\HotelReturnConfig;
 use App\Models\Returns\HotelReturns\HotelReturnItem;
 use App\Models\Returns\ReturnStatus;
-use App\Traits\SalesTrait;
+use App\Traits\PurchasesTrait;
 use Illuminate\Http\Request;
 
 class SalesPurchasesController extends Controller
 {
-    use SalesTrait;
-
+    use PurchasesTrait;
     public function index()
     {
-        $salesHotel = $this->getAllHotelSales()['return'];
-        $purchasesHotel = $this->getAllHotelSales()['return'];
-        dd($salesHotel);
-
+        $pu = $this->getHotelPurchases();
 
 
         return view('returns.queries.sales-purchases.index');
     }
+
 
 }
--- app/Traits/PurchasesTrait.php
+++ app/Traits/PurchasesTrait.php
@@ -2,34 +2,39 @@
 
 namespace App\Traits;
 
-use Exception;
-use Carbon\Carbon;
-use App\Models\Audit;
-use Illuminate\Support\Facades\Log;
+use App\Models\Returns\HotelReturns\HotelReturn;
+use App\Models\Returns\HotelReturns\HotelReturnConfig;
+use App\Models\Returns\HotelReturns\HotelReturnItem;
+use App\Models\Returns\ReturnStatus;
+use App\Models\TaxType;
 
 trait PurchasesTrait
 {
-    public function triggerAudit($modal_class, $event, $tag, $auditable_id, $old_values, $new_values)
-    {
-        $data = [
-            'auditable_id' => $auditable_id,
-            'auditable_type' => $modal_class,
-            'event' => $event,
-            'tags' => $tag,
-            'old_values' => json_encode($old_values),
-            'new_values' => json_encode($new_values),
-            'url'        => request()->fullUrl(),
-            'ip_address' => request()->getClientIp(),
-            'user_agent' => request()->userAgent(),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-            'user_id'    => auth()->user()->id,
-        ];
 
-        try {
-            Audit::create($data);
-        } catch(Exception $e){
-            Log::error($e);
-        }
+    public function getHotelPurchases()
+    {
+        $purchaseConfigs = HotelReturnConfig::query()->whereIn('code', ['LP', 'IP'])->get()->pluck('id');
+        $return = HotelReturn::class;
+        $item = HotelReturnItem::class;
+        $hotel_returns = $this->getPurchases($return, $purchaseConfigs, $item);
+        dd($hotel_returns->items);
+        return $hotel_returns;
     }
+
+    public function getPurchases($modelName, $configs, $itemModel)
+    {
+        $returnTableName = (new $modelName())->getTable();
+        $returnItems = (new $itemModel())->getTable();
+        $return = $modelName::query()
+            ->selectRaw('financial_month_id, '.$returnTableName.'.id, 
+            business_location_id, '.$returnTableName.'.business_id, '.$returnTableName.'.tax_type_id')
+            ->leftJoin(''.$returnItems.'', ''.$returnTableName.'.id', ''.$returnItems.'.return_id')
+            ->leftJoin('business_tax_type', 'business_tax_type.business_id', ''.$returnTableName.'.business_id')
+            ->where(''.$returnTableName.'.status', ReturnStatus::COMPLETE)
+            ->whereIn(''.$returnItems.'.config_id', $configs)
+            ->where('business_tax_type.status', '=','current-used')
+            ->orderByDesc(''.$returnTableName.'.id')->groupBy([''.$returnTableName.'.id'])->first();
+        return $return;
+    }
+
 }
