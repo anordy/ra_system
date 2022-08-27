13e6cf3e044fe0c879d3e0c75a621aca5605fd48
luksford <107618675+luksford@users.noreply.github.com>
2022-08-27 18:36:03 +0300

    minor changes

--- resources/views/livewire/tax-agent/actions.blade.php
+++ resources/views/livewire/tax-agent/actions.blade.php
@@ -1,7 +1,4 @@
 <div>
-{{--    {{ $taxagent->id }}--}}
-{{--    <button class="btn btn-outline-success btn-sm" wire:click="approve()"><i class="fa fa-check"></i>Approve</button>--}}
-{{--    <button class="btn btn-outline-danger btn-sm" wire:click="reject()"><i class="bi bi-x-circle-fill"></i> Reject</button>--}}
     <button type="button" class="btn btn-primary ml-2 px-3" wire:click='approve' wire:loading.attr="disabled">
         <div wire:loading wire:target="approve">
             <div class="spinner-border mr-1 spinner-border-sm text-light" role="status">
--- resources/views/livewire/tax-agent/verify-action.blade.php
+++ resources/views/livewire/tax-agent/verify-action.blade.php
@@ -1,5 +1,4 @@
 <div>
-{{--    <button class="btn btn-outline-info btn-sm" wire:click="approve()"><i class="fa fa-check"></i>Approve</button>--}}
     <button type="button" class="btn btn-primary ml-2 px-3" wire:click='approve' wire:loading.attr="disabled">
         <div wire:loading wire:target="approve">
             <div class="spinner-border mr-1 spinner-border-sm text-light" role="status">
@@ -8,7 +7,6 @@
         </div>
         <i class="fa fa-check"></i> Approve
     </button>
-{{--    <button class="btn btn-outline-danger btn-sm" wire:click="reject()"><i class="bi bi-x-circle"></i>Reject</button>--}}
     <button type="button" class="btn btn-danger ml-2 px-3" wire:click='reject' wire:loading.attr="disabled">
         <div wire:loading wire:target="reject">
             <div class="spinner-border mr-1 spinner-border-sm text-light" role="status">
