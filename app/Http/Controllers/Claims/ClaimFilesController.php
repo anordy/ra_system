d218ab4e2b6d26598d70b14105f653d325df0517
luksford <107618675+luksford@users.noreply.github.com>
2022-08-28 17:54:50 +0300

    adding permissions to controller and other minor changes

--- app/Http/Controllers/Claims/ClaimFilesController.php
+++ app/Http/Controllers/Claims/ClaimFilesController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\Claims;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Storage;
 
 class ClaimFilesController extends Controller
 {
     public function show($path)
     {
+        if (!Gate::allows('tax-claim-view')) {
+            abort(403);
+        }
 //        dd(decrypt($path));
         if ($path) {
             try {
