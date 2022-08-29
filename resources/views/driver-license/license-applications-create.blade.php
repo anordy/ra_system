b68f1aa4c5be7a1438eaa188b56673b586a0d24b
gsubx <Gozbert.Stanslaus@ubx.co.tz>
2022-08-29 16:33:17 +0300

    fixing licence

--- resources/views/driver-license/license-applications-create.blade.php
+++ resources/views/driver-license/license-applications-create.blade.php
@@ -6,8 +6,6 @@
     <div class="card mt-3">
         <div class="card-body">
             <livewire:drivers-license.wizard.wizard id="sm" >
-
-            </livewire:drivers-license.wizard.wizard>
         </div>
     </div>
 @endsection
