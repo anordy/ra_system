bdf580254364a2243e4f58cb71da122fedc24404
gsubx <Gozbert.Stanslaus@ubx.co.tz>
2022-09-01 01:39:13 +0300

    fixing issues

--- resources/views/investigation/approval/action.blade.php
+++ resources/views/investigation/approval/action.blade.php
@@ -1,2 +1,2 @@
-<a href="{{ route('tax_investigation.approvals.edit', encrypt($row->pinstance_id)) }}" class="btn btn-outline-info btn-sm" data-toggle="tooltip"
+<a href="{{ route('tax_investigation.approvals.edit', encrypt($row->id)) }}" class="btn btn-outline-info btn-sm" data-toggle="tooltip"
     data-placement="right" title="View">Review & Approve</a>
