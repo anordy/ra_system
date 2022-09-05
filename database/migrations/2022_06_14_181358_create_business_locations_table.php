bea3c8ddc7e37a9fedfd3c5a16047758d0adf85a
mangerere <107620925+mangerere@users.noreply.github.com>
2022-09-05 16:31:15 +0300

    Update 2022_06_14_181358_create_business_locations_table.php

--- database/migrations/2022_06_14_181358_create_business_locations_table.php
+++ database/migrations/2022_06_14_181358_create_business_locations_table.php
@@ -32,8 +32,8 @@ class CreateBusinessLocationsTable extends Migration
             $table->string('house_no');
             $table->string('owner_name')->nullable();
             $table->string('owner_phone_no')->nullable();
-            $table->float('pre_estimated_turnover', 20, 2);
-            $table->float('post_estimated_turnover', 20, 2);
+            $table->float('pre_estimated_turnover', 20, 2)->nullable();
+            $table->float('post_estimated_turnover', 20, 2)->nullable();
             $table->string('meter_no');
             $table->string('marking')->nullable();
             $table->dateTime('approved_on')->nullable();
