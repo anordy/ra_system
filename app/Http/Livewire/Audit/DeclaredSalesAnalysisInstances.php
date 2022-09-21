0fe3a8e6030ef28f5b28d90b0bea8824af24d18d
GeraldNjau <108731600+GeraldMarkNjau@users.noreply.github.com>
2022-09-21 09:28:35 +0300

    Multiple location & tax Type auditing

---
+++ app/Http/Livewire/Audit/DeclaredSalesAnalysisInstances.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Livewire\Audit;
+
+use App\Models\TaxAudit\TaxAuditLocation;
+use App\Models\TaxAudit\TaxAuditTaxType;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Livewire\Component;
+
+class DeclaredSalesAnalysisInstances extends Component
+{
+    use LivewireAlert;
+
+    public $audit;
+    public $locations;
+    public $taxTypes;
+
+    public function mount($audit)
+    {
+
+        $this->audit = $audit;
+        $this->locations = TaxAuditLocation::where('tax_audit_id', $audit->id)->get();
+        $this->taxTypes = TaxAuditTaxType::where('tax_audit_id', $audit->id)->get();
+        // dd($this->locations);
+    }
+
+    public function render()
+    {
+        return view('livewire.audit.declared-sales-analysis-instances');
+    }
+}
