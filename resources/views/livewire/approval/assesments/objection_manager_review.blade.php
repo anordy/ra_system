6e6ab4c5e5b3002c242b1d229649fb2fbf35c1e9
anordy-ubx <anord.john@ubx.co.tz>
2022-09-22 09:41:29 +0300

    changes in waiverobjection

--- resources/views/livewire/approval/assesments/objection_manager_review.blade.php
+++ resources/views/livewire/approval/assesments/objection_manager_review.blade.php
@@ -14,7 +14,7 @@
                     <div class="form-group col-lg-4">
                         <label class="control-label">Penalty Amount</label>
                         <input type="text" class="form-control @error('penalty') is-invalid @enderror"
-                            wire:model.lazy="penalty" disabled>
+                            wire:model.lazy="penalty" >
                         @error('penalty')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
@@ -22,7 +22,7 @@
                     <div class="form-group col-lg-4">
                         <label class="control-label">Interest Amount</label>
                         <input type="text" class="form-control @error('interest') is-invalid @enderror"
-                            wire:model.lazy="interest" disabled>
+                            wire:model.lazy="interest">
                         @error('interest')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
