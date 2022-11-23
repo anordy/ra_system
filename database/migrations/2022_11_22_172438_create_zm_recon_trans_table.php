348a4bae8d4866b1922624e75a989bf489e94e5b
gsubx <Gozbert.Stanslaus@ubx.co.tz>
2022-11-23 08:56:43 +0300

    fixing issue

---
+++ database/migrations/2022_11_22_172438_create_zm_recon_trans_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateZmReconTransTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('zm_recon_trans', function (Blueprint $table) {
+            $table->id();
+            $table->string('SpBillId', 50)->nullable();
+            $table->double('BillCtrNum',12)->nullable();
+            $table->string('pspTrxId', 200)->nullable();
+            $table->double('PaidAmt')->nullable();
+            $table->string('CCy', 3)->nullable();
+            $table->string('PayRefId', 100)->nullable();
+            $table->string('TrxDtTm',50)->nullable();
+            $table->string('CtrAccNum',15)->nullable();
+            $table->string('UsdPayChnl',50)->nullable();
+            $table->string('PspName', 100)->nullable();
+            $table->string('DptCellNum',12)->nullable();
+            $table->string('DptName',100)->nullable();
+            $table->string('DptEmailAddr',100)->nullable();
+            $table->string('Remarks',200)->nullable();
+            $table->string('ReconcRvs1',50)->nullable();
+            $table->string('ReconcRvs2',50)->nullable();
+            $table->string('ReconcRvs3',50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('zm_recon_trans');
+    }
+}
