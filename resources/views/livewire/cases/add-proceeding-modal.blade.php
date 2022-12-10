e7a125655775da422843607dfc20bd69e01de22a
kedmonmaquiz <jkedmon95@gmail.com>
2022-12-10 12:58:51 +0300

    legal cases proceeding

--- resources/views/livewire/cases/add-proceeding-modal.blade.php
+++ resources/views/livewire/cases/add-proceeding-modal.blade.php
@@ -9,9 +9,24 @@
             <div class="model-body">
                 <div class="row pr-3 pl-3">
                     <div class="form-group col-lg-12">
-                        <label class="control-label">Date</label>
+                        <label class="control-label">Date *</label>
                         <input type="date" class="form-control" wire:model.lazy="date" id="date">
                         @error('date')
+                        <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                </div>
+        
+                <div class="row pr-3 pl-3">
+                    <div class="form-group col-lg-12">
+                        <label class="control-label">Stage *</label>
+                        <select wire:model="stage_id" class="form-control" id="stage_id">
+                            <option>Choose option</option>
+                            @foreach (\App\Models\CaseStage::all() as $option)
+                                <option value="{{ $option->id }}">{{ $option->name }}</option>
+                            @endforeach
+                        </select>
+                        @error('stage_id')
                         <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
@@ -19,7 +34,7 @@
 
                 <div class="col-md-12 mb-3">
                     <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Comment</label>
+                        <label for="exampleFormControlTextarea1">Comment *</label>
                         <textarea class="form-control @error('comment') is-invalid @enderror" wire:model='comment' rows="3"></textarea>
                         @error('comment')
                         <div class="invalid-feedback">
@@ -39,21 +54,6 @@
                             @endforeach
                         </select>
                         @error('decision_id')
-                        <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                </div>
-
-                <div class="row pr-3 pl-3">
-                    <div class="form-group col-lg-12">
-                        <label class="control-label">Stage</label>
-                        <select wire:model="stage_id" class="form-control" id="stage_id">
-                            <option>Choose option</option>
-                            @foreach (\App\Models\CaseStage::all() as $option)
-                                <option value="{{ $option->id }}">{{ $option->name }}</option>
-                            @endforeach
-                        </select>
-                        @error('stage_id')
                         <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
