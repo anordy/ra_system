a6a71a503e803d13d9afd99ca737a19456e3a387
gsubx <Gozbert.Stanslaus@ubx.co.tz>
2022-12-19 16:38:28 +0300

    fixing issue

--- resources/views/livewire/approval/tax_audit.blade.php
+++ resources/views/livewire/approval/tax_audit.blade.php
@@ -45,58 +45,69 @@
                             @enderror
                         </div>
                     </div>
-                    <div class="col-lg-6 form-group">
-                        <label for="periodFrom">Auditing Period From</label>
-                        <input type="date" class="form-control @error('periodFrom') is-invalid @enderror"
-                            wire:model="periodFrom">
-                        @error('periodFrom')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="periodFrom">Auditing Period From</label>
+                            <input type="date" class="form-control @error('periodFrom') is-invalid @enderror"
+                                wire:model="periodFrom">
+                            @error('periodFrom')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="periodTo">Auditing Period To</label>
+                            <input type="date" class="form-control @error('periodTo') is-invalid @enderror"
+                                wire:model="periodTo">
+                            @error('periodTo')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="intension">Intension</label>
+                            <textarea class="form-control" wire:model.lazy="intension" id="intension" rows="3"></textarea>
+                            @error('intension')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="periodTo">Scope</label>
+                            <textarea class="form-control" wire:model.lazy="scope" id="scope" rows="3"></textarea>
+                            @error('scope')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
                     </div>
                     <div class="col-lg-6 form-group">
-                        <label for="periodTo">Auditing Period To</label>
-                        <input type="date" class="form-control @error('periodTo') is-invalid @enderror"
-                            wire:model="periodTo">
-                        @error('periodTo')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-                    <div class="col-lg-6 form-group">
-                        <label for="intension">Intension</label>
-                        <textarea class="form-control" wire:model.lazy="intension" id="intension" rows="3"></textarea>
-                        @error('intension')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-                    <div class="col-lg-6 form-group">
-                        <label for="periodTo">Scope</label>
-                        <textarea class="form-control" wire:model.lazy="scope" id="scope" rows="3"></textarea>
-                        @error('scope')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
-                    </div>
-                    <div class="col-lg-6 form-group">
-                        <label for="auditingDate">Date of auditing</label>
-                        <input type="date" class="form-control @error('auditingDate') is-invalid @enderror"
-                            wire:model="auditingDate">
-                        @error('auditingDate')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
+                        <div class="form-group">
+                            <label for="auditingDate">Date of auditing</label>
+                            <input type="date" class="form-control @error('auditingDate') is-invalid @enderror"
+                                wire:model="auditingDate">
+                            @error('auditingDate')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
                     </div>
                 </div>
             @endif
             @if ($this->checkTransition('conduct_audit'))
-                <div class="row p-0">
+                <div class="row pl-3 pr-3">
                     <div class="form-group col-lg-6">
                         <label class="control-label">Preliminary report</label>
                         <input type="file" class="form-control  @error('preliminaryReport') is-invalid @enderror"
@@ -139,7 +150,7 @@
                     <div class="form-group col-lg-6">
                         <label for="exampleFormControlTextarea1">Has Notice of Asessement</label>
                         <select class="form-control @error('hasAssessment') is-invalid @enderror"
-                            wire:model="hasAssessment"  wire:change="hasNoticeOfAttachmentChange($event.target.value)" >
+                            wire:model="hasAssessment" wire:change="hasNoticeOfAttachmentChange($event.target.value)">
                             <option value='' selected>Select</option>
                             <option value=1>Yes</option>
                             <option value=0>No</option>
@@ -153,7 +164,8 @@
                     @if ($hasAssessment)
                         <div class="form-group col-lg-6">
                             <label class="control-label">Principal Amount</label>
-                            <input x-data x-mask:dynamic="$money($input)" type="text" class="form-control @error('principalAmount') is-invalid @enderror"
+                            <input x-data x-mask:dynamic="$money($input)" type="text"
+                                class="form-control @error('principalAmount') is-invalid @enderror"
                                 wire:model.lazy="principalAmount">
                             @error('principalAmount')
                                 <span class="text-danger">{{ $message }}</span>
@@ -161,7 +173,8 @@
                         </div>
                         <div class="form-group col-lg-6">
                             <label class="control-label">Interest Amount</label>
-                            <input x-data x-mask:dynamic="$money($input)" type="text" class="form-control @error('interestAmount') is-invalid @enderror"
+                            <input x-data x-mask:dynamic="$money($input)" type="text"
+                                class="form-control @error('interestAmount') is-invalid @enderror"
                                 wire:model.lazy="interestAmount">
                             @error('interestAmount')
                                 <span class="text-danger">{{ $message }}</span>
@@ -169,7 +182,8 @@
                         </div>
                         <div class="form-group col-lg-6">
                             <label class="control-label">Penalty Amount</label>
-                            <input x-data x-mask:dynamic="$money($input)" type="text" class="form-control @error('penaltyAmount') is-invalid @enderror"
+                            <input x-data x-mask:dynamic="$money($input)" type="text"
+                                class="form-control @error('penaltyAmount') is-invalid @enderror"
                                 wire:model.lazy="penaltyAmount">
                             @error('penaltyAmount')
                                 <span class="text-danger">{{ $message }}</span>
@@ -200,22 +214,26 @@
             </div>
         @elseif ($this->checkTransition('assign_officers'))
             <div class="modal-footer p-2 m-0">
-                <button type="button" class="btn btn-primary" wire:click="confirmPopUpModal('approve', 'assign_officers')">
+                <button type="button" class="btn btn-primary"
+                    wire:click="confirmPopUpModal('approve', 'assign_officers')">
                     Assign & Forward
                 </button>
             </div>
         @elseif ($this->checkTransition('conduct_audit'))
             <div class="modal-footer p-2 m-0">
-                <button type="button" class="btn btn-primary" wire:click="confirmPopUpModal('approve', 'conduct_audit')">
+                <button type="button" class="btn btn-primary"
+                    wire:click="confirmPopUpModal('approve', 'conduct_audit')">
                     Approve & Forward
                 </button>
             </div>
         @elseif ($this->checkTransition('preliminary_report'))
             <div class="modal-footer p-2 m-0">
-                <button type="button" class="btn btn-danger" wire:click="confirmPopUpModal('reject', 'correct_preliminary_report')">
+                <button type="button" class="btn btn-danger"
+                    wire:click="confirmPopUpModal('reject', 'correct_preliminary_report')">
                     Reject & Return Back
                 </button>
-                <button type="button" class="btn btn-primary" wire:click="confirmPopUpModal('approve', 'preliminary_report')">
+                <button type="button" class="btn btn-primary"
+                    wire:click="confirmPopUpModal('approve', 'preliminary_report')">
                     Approve & Forward
                 </button>
             </div>
@@ -225,37 +243,44 @@
                     wire:click="confirmPopUpModal('reject', 'correct_preliminary_report_review')">
                     Reject & Return Back
                 </button>
-                <button type="button" class="btn btn-primary" wire:click="confirmPopUpModal('approve', 'preliminary_report_review')">
+                <button type="button" class="btn btn-primary"
+                    wire:click="confirmPopUpModal('approve', 'preliminary_report_review')">
                     Approve & Forward
                 </button>
             </div>
         @elseif ($this->checkTransition('prepare_final_report'))
             <div class="modal-footer p-2 m-0">
-                <button type="button" class="btn btn-primary" wire:click="confirmPopUpModal('approve', 'prepare_final_report')">
+                <button type="button" class="btn btn-primary"
+                    wire:click="confirmPopUpModal('approve', 'prepare_final_report')">
                     Approve & Forward
                 </button>
             </div>
         @elseif ($this->checkTransition('final_report'))
             <div class="modal-footer p-2 m-0">
-                <button type="button" class="btn btn-danger" wire:click="confirmPopUpModal('reject', 'correct_final_report')">
+                <button type="button" class="btn btn-danger"
+                    wire:click="confirmPopUpModal('reject', 'correct_final_report')">
                     Reject & Return Back
                 </button>
-                <button type="button" class="btn btn-primary" wire:click="confirmPopUpModal('approve', 'final_report')">
+                <button type="button" class="btn btn-primary"
+                    wire:click="confirmPopUpModal('approve', 'final_report')">
                     Approve & Forward
                 </button>
             </div>
         @elseif ($this->checkTransition('final_report_review'))
             <div class="modal-footer p-2 m-0">
-                <button type="button" class="btn btn-danger" wire:click="confirmPopUpModal('reject', 'correct_final_report_review')">
+                <button type="button" class="btn btn-danger"
+                    wire:click="confirmPopUpModal('reject', 'correct_final_report_review')">
                     Reject & Return Back
                 </button>
-                <button type="button" class="btn btn-primary" wire:click="confirmPopUpModal('approve', 'final_report_review')">
+                <button type="button" class="btn btn-primary"
+                    wire:click="confirmPopUpModal('approve', 'final_report_review')">
                     Approve & Forward
                 </button>
             </div>
         @elseif ($this->checkTransition('accepted'))
             <div class="modal-footer p-2 m-0">
-                <button type="button" class="btn btn-danger" wire:click="confirmPopUpModal('reject', 'rerejectjected')">
+                <button type="button" class="btn btn-danger"
+                    wire:click="confirmPopUpModal('reject', 'rerejectjected')">
                     Reject & Return Back
                 </button>
                 <button type="button" class="btn btn-primary" wire:click="confirmPopUpModal('approve', 'accepted')">
