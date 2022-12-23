a3336f10436a7f7fbbad8c0ea1b7a140a4eff70c
gsubx <Gozbert.Stanslaus@ubx.co.tz>
2022-12-23 18:21:26 +0300

    notificaiton paginations

--- resources/views/livewire/notifications/notifications-table.blade.php
+++ resources/views/livewire/notifications/notifications-table.blade.php
@@ -1,11 +1,12 @@
-<div>
     <div>
-        <div class="text-left"><button class="btn btn-sm btn-outline-danger mb-2" @if($notifications->isEmpty()) disabled @endif wire:click="deleteSelected">Delete
-                Selected</button></div>
-        <table class="table table-hover table-bordered">
+        <div class="text-left"><button class="btn btn-sm btn-outline-danger mb-2"
+                @if ($notifications->isEmpty()) disabled @endif wire:click="deleteSelected">Delete
+                Selected</button>
+        </div>
+        <table class="table table-hover table-bordered table-sm">
             <thead>
                 <tr>
-                    <th><input type="checkbox" @if($notifications->isEmpty()) disabled @endif></th>
+                    <th><input type="checkbox" @if ($notifications->isEmpty()) disabled @endif></th>
                     <th>Time</th>
                     <th>Subject</th>
                     <th>Message</th>
@@ -13,29 +14,30 @@
                 </tr>
             </thead>
             <tbody>
-                @if($notifications->isNotEmpty())
-                @foreach ($notifications as $row)
-                <tr>
-                    <td><input type="checkbox" wire:model.defer="selectedItems.{{ $row->id }}"></td>
-                    <td>{!! $row->seen <= 1 ? '<strong>' : '' !!}{{ $row->created_at->diffForHumans() ?? '' }}{!!
-                            $row->seen <= 1 ? '</strong>' : '' !!}</td>
-                    <td>{!! $row->seen <= 1 ? '<strong>' : '' !!}{{ $row->data->subject ?? '' }}{!! $row->seen <= 1 ?
-                            '</strong>' : '' !!}</td>
-                    <td>{!! $row->seen <= 1 ? '<strong>' : '' !!}{{ $row->data->message ?? '' }}{!! $row->seen <= 1 ?
-                            '</strong>' : '' !!}</td>
-                    <td>
-                        <button class="btn btn-sm btn-outline-success" wire:click="read({{$row}})">
-                            {!! $row->seen <= 1 ? '<strong>view</strong>' : 'view' !!}
-                        </button>
-                    </td>
-                </tr>
-                @endforeach
+                @if ($notifications->isNotEmpty())
+                    @foreach ($notifications as $row)
+                        <tr>
+                            <td><input type="checkbox" wire:model.defer="selectedItems.{{ $row->id }}"></td>
+                            <td>{!! $row->seen <= 1 ? '<strong>' : '' !!}{{ $row->created_at->diffForHumans() ?? '' }}{!! $row->seen <= 1 ? '</strong>' : '' !!}
+                            </td>
+                            <td>{!! $row->seen <= 1 ? '<strong>' : '' !!}{{ $row->data->subject ?? '' }}{!! $row->seen <= 1 ? '</strong>' : '' !!}</td>
+                            <td>{!! $row->seen <= 1 ? '<strong>' : '' !!}{{ $row->data->message ?? '' }}{!! $row->seen <= 1 ? '</strong>' : '' !!}</td>
+                            <td>
+                                <button class="btn btn-sm btn-outline-success" wire:click="read({{ $row }})">
+                                    {!! $row->seen <= 1 ? '<strong>view</strong>' : 'view' !!}
+                                </button>
+                            </td>
+                        </tr>
+                    @endforeach
                 @else
-                <tr>
-                    <td colspan="5" class="text-center">There are no new notifications for you at the moment</td>
-                </tr>
+                    <tr>
+                        <td colspan="5" class="text-center">There are no new notifications for you at the moment</td>
+                    </tr>
                 @endif
             </tbody>
+           
         </table>
+        <div class="d-flex justify-content-end">
+            {{ $notifications->links() }}
+        </div>
     </div>
-</div>
