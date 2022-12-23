37c5097b6eab5e4824d0ffdf9aecf5ccc67e8961
gsubx <Gozbert.Stanslaus@ubx.co.tz>
2022-12-23 11:29:12 +0300

    approval progress on branches

--- resources/views/business/branches/index.blade.php
+++ resources/views/business/branches/index.blade.php
@@ -11,15 +11,19 @@
         </div>
         <div class="card-body mt-0 p-2">
             <nav class="nav nav-tabs mt-0 border-top-0">
-                <a href="#tab2" class="nav-item nav-link font-weight-bold active">Pending Approval</a>
-                <a href="#tab1" class="nav-item nav-link font-weight-bold">Approved Branches</a>
+                <a href="#tab2" class="nav-item nav-link font-weight-bold active">Approved Branches</a>
+                <a href="#tab1" class="nav-item nav-link font-weight-bold">Pending Approval</a>
+                <a href="#tab3" class="nav-item nav-link font-weight-bold">Approval Progress</a>
             </nav>
             <div class="tab-content px-2 card pt-3 pb-2">
                 <div id="tab2" class="tab-pane fade m-2 show active">
-                    <livewire:business.branches-approval-table status='pending'></livewire:business.branches-approval-table>
+                    <livewire:business.branches-table status='approved'></livewire:business.branches-table>
                 </div>
                 <div id="tab1" class="tab-pane fade m-2">
-                    <livewire:business.branches-table status='approved'></livewire:business.branches-table>
+                    @livewire('business.branches-approval-table')
+                </div>
+                <div id="tab3" class="tab-pane fade m-2">
+                    @livewire('business.branches-approval-progress-table')
                 </div>
             </div>
         </div>
@@ -35,4 +39,3 @@
         });
     </script>
 @endsection
-
