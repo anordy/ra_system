ac2e71d938d40af64c36ae081aec1db023c2fad8
kedmonmaquiz <jkedmon95@gmail.com>
2022-12-23 12:25:10 +0300

    Mfumo uwe na uwezo wa kuchagua tarehe husika kwa kila Unpaid (date range)

--- app/Http/Livewire/Payments/PaymentFilter.php
+++ app/Http/Livewire/Payments/PaymentFilter.php
@@ -15,19 +15,34 @@ class PaymentFilter extends Component
     public $currency;
     public $tax_types;
     public $tax_type_id;
+    public $range_start;
+    public $range_end;
+
+    protected $rules =[
+        'currency' => 'required',
+        'tax_type_id' => 'required',
+        'range_start' => 'required',
+        'range_end' => 'required'
+    ];
 
     public function mount($tablename)
     {
         $this->tax_types = TaxType::where('category', 'main')->get();
         $this->tableName = $tablename;
+        $this->range_start = date('Y-m-d', strtotime(now()));
+        $this->range_end = date('Y-m-d', strtotime(now()));
     }
     
     public function fillter()
     {
+        $this->validate();
         $filters = [
             'tax_type_id' => $this->tax_type_id,
             'currency'    => $this->currency,
+            'range_start' => date('Y-m-d 00:00:00', strtotime($this->range_start)),
+            'range_end'   => date('Y-m-d 23:59:59', strtotime($this->range_end)),
         ];
+
 
         $this->emitTo('App\Http\Livewire\Payments\PendingPaymentsTable', 'filterData', $filters);
         $this->data = $filters;
@@ -46,8 +61,12 @@ class PaymentFilter extends Component
         if (isset($data['currency']) && $data['currency'] != 'All') {
             $filter->Where('currency', $data['currency']);
         }
+        if (isset($data['range_start']) && isset($data['range_end'])) {
+            $filter->WhereBetween('created_at', [$data['range_start'],$data['range_end']]);
+        }
 
         $records  = $filter->whereIn('status', [PaymentStatus::PENDING])->orderBy('created_at', 'DESC')->get();
+
         $fileName = 'pending_payments' . '_' . $data['currency'] . '.pdf';
         $title = 'pending_payments' . '_' . $data['currency'] . '.pdf';
 
