8a1782b5a0abce7fea745eb6466df62abc6e3109
luksford <107618675+luksford@users.noreply.github.com>
2022-12-26 20:06:58 +0300

    improvement

--- resources/views/taxagents/verification-request-agent-show.blade.php
+++ resources/views/taxagents/verification-request-agent-show.blade.php
@@ -28,7 +28,7 @@
                         <div id="tab1" class="tab-pane fade active show">
                             @include('taxagents.includes.show')
                             @if(empty($fee))
-                                <div class="mx-1 p-2">
+                                <div class="mx-3 p-2">
                                     <div class="row py-2 alert alert-danger rounded-0 shadow-sm border-danger">
                                         <div class="col-md-6">
                                             <span class="font-weight-bold text-uppercase">Notice</span>
@@ -58,7 +58,86 @@
                         </div>
 
                         <div id="tab3" class="tab-pane fade">
-                            dmfnnffff
+                            <div class="p-5">
+                                @if(count($agent->request) > 0)
+                                    @foreach($agent->request as $request)
+                                        <div class="card p-2 mb-3">
+                                            <div class="card-header">
+                                                <h6>Renew Request for {{ date('Y',strtotime($request->created_at)) }}</h6>
+                                            </div>
+                                            <div class="card-body mt-0 p-2 mb-2">
+                                                <div class="row">
+                                                    <div class="col-md-3 mb-2">
+                                                        <span class="font-weight-bold text-uppercase">Request Created At</span>
+                                                        <p class="my-1">{{ date('D, d-m-Y',strtotime($request->created_at)) }}</p>
+                                                    </div>
+
+                                                    <div class="col-md-3 mb-2">
+                                                        <span class="font-weight-bold text-uppercase">Request Status</span>
+                                                        <p class="my-1">
+
+                                                            @if($request->status == \App\Models\TaxAgentStatus::PENDING)
+                                                                <span class="badge badge-danger py-1 px-2"
+                                                                      style="border-radius: 1rem; background: #dc354559; color: #cf1c2d; font-size: 85%"><i
+                                                                            class="bi bi-clock-history mr-1"></i>Pending</span>
+
+                                                            @elseif($request->status == \App\Models\TaxAgentStatus::APPROVED)
+                                                                <span class="badge badge-success py-1 px-2"
+                                                                      style="border-radius: 1rem; background: #72DC3559; color: #319e0a; font-size: 85%"><i
+                                                                            class="bi bi-check-circle-fill mr-1"></i>Approved</span>
+                                                            @elseif($request->status == \App\Models\TaxAgentStatus::VERIFIED)
+                                                                <span class="badge badge-success py-1 px-2"
+                                                                      style="border-radius: 1rem; background: #72DC3559; color: #319e0a; font-size: 85%"><i
+                                                                            class="bi bi-check-circle-fill mr-1"></i>Verified</span>
+                                                            @else
+                                                                <span class="badge badge-danger py-1 px-2"
+                                                                      style="border-radius: 1rem; background: #dc354559; color: #cf1c2d; font-size: 85%"><i
+                                                                            class="bi bi-x-circle-fill mr-1"></i>Rejected</span>
+                                                            @endif
+                                                        </p>
+                                                    </div>
+
+                                                    <div class="col-md-3 mb-2">
+                                                        <span class="font-weight-bold text-uppercase">Renew Payment</span>
+                                                        <p>
+                                                            @if ($request->bill != null)
+                                                                @if ($request->bill->status == 'paid')
+                                                                    <span style=" background: #72DC3559; color: #319e0a; font-size: 85%"
+                                                                          class="badge badge-success p-2">Paid</span>
+                                                                @else
+                                                                    <span style=" background: #dc354559; color: #cf1c2d; font-size: 85%"
+                                                                          class="badge badge-danger p-2">Not Paid</span>
+                                                                @endif
+                                                            @else
+                                                                <span style=" background: #dc354559; color: #cf1c2d; font-size: 85%"
+                                                                      class="badge badge-danger p-2">Not Paid</span>
+                                                            @endif
+                                                        </p>
+                                                    </div>
+
+                                                    <div class="col-md-3 mb-2">
+                                                        <span class="font-weight-bold text-uppercase">Request Expire Date</span>
+                                                        <p class="my-1">{{ date('D, d-m-Y',strtotime($request->renew_expire_date)) }}</p>
+                                                    </div>
+
+                                                </div>
+                                                <hr>
+
+                                                <div>
+                                                    <h6 class="text-capitalize">Approval history</h6>
+                                                    <livewire:tax-agent.approval.registration.approval-history-table
+                                                            modelName='App\Models\RenewTaxAgentRequest'
+                                                            modelId="{{ encrypt($request->id) }}"/>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                @else
+                                    <div class="alert alert-info text-center">
+                                        No renew requests from the selected taxpayer
+                                    </div>
+                                @endif
+                            </div>
                         </div>
 
                     </div>
