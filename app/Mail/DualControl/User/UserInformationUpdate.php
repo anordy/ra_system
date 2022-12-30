711962b7e227bb2b163e4fb20eb56f61955f4cbe
GeraldNjau <108731600+GeraldMarkNjau@users.noreply.github.com>
2022-12-30 15:39:13 +0300

    Email implementation to notify User when updating ZRB Staff informations

---
+++ app/Mail/DualControl/User/UserInformationUpdate.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail\DualControl\User;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class UserInformationUpdate extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $payload;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($payload)
+    {
+        //
+        $this->payload = $payload;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.dual-control.user-notification')
+        ->subject('Important: Staff Account Information Update');
+    }
+}
