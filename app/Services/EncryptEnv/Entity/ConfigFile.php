bf24ea77354e9ea4be184a3de94c52e2f8fe1c01
gsubx <Gozbert.Stanslaus@ubx.co.tz>
2022-12-30 14:36:31 +0300

    encryptions on env

---
+++ app/Services/EncryptEnv/Entity/ConfigFile.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Services\EncryptEnv\Entity;
+
+
+class ConfigFile
+{
+    /**
+     * @var array
+     */
+    protected $configarr;
+
+    /**
+     * @var array
+     */
+    protected $encenv_config;
+
+    /**
+     * @var string
+     */
+    protected $configpath;
+
+    /**
+     * @var string
+     */
+    protected $configoutput;
+
+    /**
+     * @var string
+     */
+    protected $configfile;
+
+
+    function __construct()
+    {
+        if (file_exists(config_path('envsecure.php'))) {
+            $encenv_config = require(config_path('envsecure.php'));
+            $this->encenv_config = $encenv_config;
+        }
+        $this->configfile = !empty($encenv_config['custom_config_file']) ? $encenv_config['custom_config_file'] : null;
+
+        !empty($encenv_config['custom_config_file']) && $this->configpath = config_path($encenv_config['custom_config_file']);
+
+
+        $this->configarr = !empty($this->configpath) && file_exists($this->configpath) ? require $this->configpath : (!empty($_ENV) ? $_ENV : getenv());
+    
+
+        !empty($this->configpath) && !empty($encenv_config['custom_config_output']) ?
+            $this->configoutput = $encenv_config['custom_config_output'] :
+            $this->configoutput = 'env';
+
+        empty($this->configpath) && $this->configpath = base_path('/.env');
+    }
+
+    /**
+     * Get Config File properties as array
+     *
+     * @return array
+     */
+    public function get()
+    {
+        return $this->configarr;
+    }
+
+    /**
+     * Get Package Config properties as array
+     *
+     * @return array
+     */
+    public function getEncEnvConfig()
+    {
+        return $this->encenv_config;
+    }
+
+    /**
+     * Get Config File Path
+     *
+     * @return string
+     */
+    public function getConfigPath()
+    {
+        return $this->configpath;
+    }
+
+    /**
+     * Get Config Output Format
+     *
+     * @return string
+     */
+    public function getOutputFormat()
+    {
+        return $this->configoutput;
+    }
+
+    /**
+     * Get Config Filename
+     *
+     * @return string
+     */
+    public function getFilename()
+    {
+        return $this->configfile;
+    }
+}
