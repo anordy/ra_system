e4dd8c3678e29a1f6b052fa5ff948fe9bfe9d670
luksford <107618675+luksford@users.noreply.github.com>
2023-01-01 21:26:47 +0300

    migration for vat attachments

---
+++ database/migrations/2023_01_01_204117_create_vat_zero_rated_sales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVatZeroRatedSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vat_zero_rated_sales', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('financial_month_id');
+            $table->unsignedBigInteger('vat_return_id')->nullable();
+            $table->string('receipt_number');
+            $table->timestamp('receipt_date');
+            $table->decimal('amount',20,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vat_zero_rated_sales');
+    }
+}
