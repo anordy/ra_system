6719ccdb2248e85f526dc59c9ec79c1c727f9d0d
luksford <107618675+luksford@users.noreply.github.com>
2023-01-03 10:02:23 +0300

    moving dual control activities link from settings to system

--- resources/views/layouts/component/sidebar.blade.php
+++ resources/views/layouts/component/sidebar.blade.php
@@ -941,11 +941,7 @@
                             <a href="{{ route('settings.zrb-bank-accounts.index') }}">ZRB Bank Accounts</a>
                         </li>
                     @endcan
-                    {{--                    @can('setting-dual-control-activities') --}}
-                    <li class="{{ request()->is('settings/dual-control-activities/*') ? 'active' : '' }}">
-                        <a href="{{ route('settings.dual-control-activities.index') }}">Dual Control Activities</a>
-                    </li>
-                    {{--                    @endcan --}}
+
                 </ul>
             </li>
         @endcan
@@ -964,6 +960,12 @@
                             <a href="{{ route('system.workflow.index') }}">Workflow Configure</a>
                         </li>
                     @endcan
+
+                        {{--                    @can('setting-dual-control-activities') --}}
+                        <li class="{{ request()->is('system/dual-control-activities/*') ? 'active' : '' }}">
+                            <a href="{{ route('system.dual-control-activities.index') }}">Dual Control Activities</a>
+                        </li>
+                        {{--                    @endcan --}}
                     {{-- @can('system-all-pdfs-view')
                         <li class="{{ request()->is('system/workflow*') ? 'active' : '' }}">
                             <a href="{{ route('pdf.all') }}">All PDF's</a>
--- routes/web.php
+++ routes/web.php
@@ -201,8 +201,7 @@ Route::middleware(['2fa', 'auth'])->group(function () {
         Route::resource('/transaction-fees', TransactionFeeController::class);
 
         Route::get('/approval-levels', [ApprovalLevelController::class, 'index'])->name('approval-levels.index');
-        Route::get('/dual-control-activities', [DualControlActivityController::class, 'index'])->name('dual-control-activities.index');
-        Route::get('/dual-control-activities/show/{id}', [DualControlActivityController::class, 'show'])->name('dual-control-activities.show');
+
     });
 
     Route::get('/bill_invoice/pdf/{id}', [QRCodeGeneratorController::class, 'invoice'])->name('bill.invoice');
@@ -219,6 +218,8 @@ Route::middleware(['2fa', 'auth'])->group(function () {
     Route::prefix('system')->name('system.')->group(function () {
         Route::resource('audits', AuditController::class);
         Route::resource('workflow', WorkflowController::class);
+        Route::get('/dual-control-activities', [DualControlActivityController::class, 'index'])->name('dual-control-activities.index');
+        Route::get('/dual-control-activities/show/{id}', [DualControlActivityController::class, 'show'])->name('dual-control-activities.show');
     });
 
     Route::prefix('taxpayers')->as('taxpayers.')->group(function () {
