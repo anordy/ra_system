f1f778ecbe029bd71893a5f7b691e96ef79cf862
luksford <107618675+luksford@users.noreply.github.com>
2023-01-03 17:05:36 +0300

    improvement

--- resources/views/dual-control-activities/details/user-role.blade.php
+++ resources/views/dual-control-activities/details/user-role.blade.php
@@ -7,37 +7,120 @@
         <div class="card-body">
             <table class="table table-striped table-sm table-bordered">
                 <thead>
-                    <th style="width: 18%">Property</th>
-                    @if ($new_values)
-                        <th style="width: 37%">Old Data</th>
-                        <th style="width: 35%">New Data</th>
-                        <th style="width: 10%">Status</th>
-                    @else
-                        <th style="width: 82%">Data</th>
-                    @endif
+                <th style="width: 18%">Property</th>
+                @if ($new_values)
+                    <th style="width: 37%">Old Data</th>
+                    <th style="width: 35%">New Data</th>
+                    <th style="width: 10%">Status</th>
+                @else
+                    <th style="width: 82%">Data</th>
+                @endif
                 </thead>
                 <tbody>
-                    <tr>
-                        <th>Role</th>
+                <tr>
+                    <th>First Name</th>
+                    <td>
+                        <p class="my-1">
+                            {{ $result->action != \App\Models\DualControl::EDIT ? $data->fname : $old_values->fname }}
+                        </p>
+                    </td>
+                    @if ($new_values)
                         <td>
-                            <p class="my-1">
-                                {{ $result->action != \App\Models\DualControl::EDIT ? getRole($data->role_id): getRole($old_values->role_id) }}
-                            </p>
+                            {{ $new_values->fname ?? '' }}
                         </td>
-                        @if ($new_values)
-                            <td>
-                                {{ getRole($new_values->role_id) ?? '' }}
-                            </td>
 
-                            @if (compareDualControlValues(
-                                $result->action != \App\Models\DualControl::EDIT ? getRole($data->role_id): getRole($old_values->role_id),
-                                getRole($new_values->role_id)))
-                                <td class="table-success">NOT CHANGED</td>
-                            @else
-                                <td class="table-danger">CHANGED</td>
-                            @endif
+                        @if (compareDualControlValues(
+                            $result->action != \App\Models\DualControl::EDIT ? $data->fname : $old_values->fname, $new_values->fname))
+                            <td class="table-success">NOT CHANGED</td>
+                        @else
+                            <td class="table-danger">CHANGED</td>
                         @endif
-                    </tr>
+                    @endif
+                </tr>
+
+                <tr>
+                    <th>Last Name</th>
+                    <td>
+                        <p class="my-1">
+                            {{ $result->action != \App\Models\DualControl::EDIT ? $data->lname : $old_values->lname }}
+                        </p>
+                    </td>
+                    @if ($new_values)
+                        <td>
+                            {{ $new_values->lname ?? '' }}
+                        </td>
+
+                        @if (compareDualControlValues(
+                            $result->action != \App\Models\DualControl::EDIT ? $data->lname : $old_values->lname, $new_values->lname))
+                            <td class="table-success">NOT CHANGED</td>
+                        @else
+                            <td class="table-danger">CHANGED</td>
+                        @endif
+                    @endif
+                </tr>
+
+                <tr>
+                    <th>Phone Number</th>
+                    <td>
+                        <p class="my-1">
+                            {{ $result->action != \App\Models\DualControl::EDIT ? $data->phone : $old_values->phone }}
+                        </p>
+                    </td>
+                    @if ($new_values)
+                        <td>
+                            {{ $new_values->phone ?? '' }}
+                        </td>
+
+                        @if (compareDualControlValues(
+                            $result->action != \App\Models\DualControl::EDIT ? $data->phone : $old_values->phone, $new_values->phone))
+                            <td class="table-success">NOT CHANGED</td>
+                        @else
+                            <td class="table-danger">CHANGED</td>
+                        @endif
+                    @endif
+                </tr>
+
+                <tr>
+                    <th>Email</th>
+                    <td>
+                        <p class="my-1">
+                            {{ $result->action != \App\Models\DualControl::EDIT ? $data->email : $old_values->email }}
+                        </p>
+                    </td>
+                    @if ($new_values)
+                        <td>
+                            {{ $new_values->email ?? '' }}
+                        </td>
+
+                        @if (compareDualControlValues(
+                            $result->action != \App\Models\DualControl::EDIT ? $data->email : $old_values->email, $new_values->email))
+                            <td class="table-success">NOT CHANGED</td>
+                        @else
+                            <td class="table-danger">CHANGED</td>
+                        @endif
+                    @endif
+                </tr>
+                <tr>
+                    <th>Role</th>
+                    <td>
+                        <p class="my-1">
+                            {{ $result->action != \App\Models\DualControl::EDIT ? getRole($data->role_id): getRole($old_values->role_id) }}
+                        </p>
+                    </td>
+                    @if ($new_values)
+                        <td>
+                            {{ getRole($new_values->role_id) ?? '' }}
+                        </td>
+
+                        @if (compareDualControlValues(
+                            $result->action != \App\Models\DualControl::EDIT ? getRole($data->role_id): getRole($old_values->role_id),
+                            getRole($new_values->role_id)))
+                            <td class="table-success">NOT CHANGED</td>
+                        @else
+                            <td class="table-danger">CHANGED</td>
+                        @endif
+                    @endif
+                </tr>
                 </tbody>
             </table>
         </div>
