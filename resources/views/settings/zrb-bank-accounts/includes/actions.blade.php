c90656ce8480fb93ac55ca2e8c36607b2630b539
GeraldNjau <108731600+GeraldMarkNjau@users.noreply.github.com>
2023-01-10 15:01:00 +0300

    Check Maker on edit and delete actions

--- resources/views/settings/zrb-bank-accounts/includes/actions.blade.php
+++ resources/views/settings/zrb-bank-accounts/includes/actions.blade.php
@@ -1,9 +1,11 @@
 <div>
     @if ($row->is_approved == 1)
-        <button class="btn btn-info btn-sm"
-            onclick="Livewire.emit('showModal', 'settings.zrb-banks.zrb-bank-account-edit-modal','{{ encrypt($row->id) }}')"><i
-                class="fa fa-edit"></i> </button>
-        <button class="btn btn-danger btn-sm" wire:click="delete('{{ encrypt($row->id) }}')"><i class="fa fa-trash"></i>
-        </button>
+        @if (approvalLevel(Auth::user()->level_id, 'Maker'))
+            <button class="btn btn-info btn-sm"
+                onclick="Livewire.emit('showModal', 'settings.zrb-banks.zrb-bank-account-edit-modal','{{ encrypt($row->id) }}')"><i
+                    class="fa fa-edit"></i> </button>
+            <button class="btn btn-danger btn-sm" wire:click="delete('{{ encrypt($row->id) }}')"><i class="fa fa-trash"></i>
+            </button>
+        @endif
     @endif
 </div>
