b71b2b233075ac6a5fb03db5feb197fb21f1d1c0
GeraldNjau <108731600+GeraldMarkNjau@users.noreply.github.com>
2023-01-13 20:32:33 +0300

    encrypt ids

--- resources/views/relief/project_list/index.blade.php
+++ resources/views/relief/project_list/index.blade.php
@@ -14,7 +14,7 @@
             <h5 class="text-uppercase">Projects List For {{ $project->name }}</h5>
             <div class="card-tools">
                 <button class="btn btn-info btn-sm"
-                    onclick="Livewire.emit('showModal', 'relief.relief-project-list-add-modal', {{ $project->id }})"><i
+                    onclick="Livewire.emit('showModal', 'relief.relief-project-list-add-modal', {{ encrypt($project->id) }})"><i
                         class="fa fa-plus-circle"></i>
                     Add</button>
             </div>
