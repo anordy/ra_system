212a63188ee62a627a78ba9294524e7e36eb6cfb
GeraldNjau <108731600+GeraldMarkNjau@users.noreply.github.com>
2023-01-13 10:26:08 +0300

    kyc amendment details

--- app/Http/Livewire/Kyc/KycAmendmentRequestAddModal.php
+++ app/Http/Livewire/Kyc/KycAmendmentRequestAddModal.php
@@ -70,7 +70,6 @@ class KycAmendmentRequestAddModal extends Component
         $this->id_type = $this->kyc->id_type;
         $this->countries = Country::select('id', 'nationality')->where('name', '!=', 'Tanzania')->get();
         $this->regions = Region::where('is_approved', DualControl::APPROVE)->select('id', 'name')->get();
-//        dd($this->countries->toArray());
         $this->districts = District::where('region_id', $this->region)->where('is_approved', DualControl::APPROVE)->select('id', 'name')->get();
         $this->wards = Ward::where('district_id', $this->district)->where('is_approved', DualControl::APPROVE)->select('id', 'name')->get();
         $this->streets = Street::where('ward_id', $this->ward)->where('is_approved', DualControl::APPROVE)->select('id', 'name')->get();
