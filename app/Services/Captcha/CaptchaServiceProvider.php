a43161c7dd8c01e61182824767aca8ffa1462566
gsubx <Gozbert.Stanslaus@ubx.co.tz>
2023-01-19 14:04:07 +0300

    fixing captcha

--- app/Services/Captcha/CaptchaServiceProvider.php
+++ app/Services/Captcha/CaptchaServiceProvider.php
@@ -11,6 +11,27 @@ use Illuminate\Support\ServiceProvider;
 class CaptchaServiceProvider extends ServiceProvider
 {
     /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        /* @var Factory $validator */
+        $validator = $this->app['validator'];
+
+        // Validator extensions
+        $validator->extend('captcha', function ($attribute, $value, $parameters) {
+            return captcha_check($value);
+        });
+
+        // Validator extensions
+        $validator->extend('captcha_api', function ($attribute, $value, $parameters) {
+            return captcha_api_check($value, $parameters[0], $parameters[1] ?? 'default');
+        });
+    }
+
+    /**
      * Register the service provider.
      *
      * @return void
