60c82350b8b82127837ab2955018ce743400e8fb
Meshack Fungo <meshack.fungo@ubx.co.tz>
2023-01-30 22:05:57 +0300

    Minor fixes

--- app/Enum/InvoiceReconStatus.php
+++ app/Enum/InvoiceReconStatus.php
@@ -4,7 +4,7 @@ namespace App\Enum;
 
 use ReflectionClass;
 
-class BankReconStatus implements Status
+class InvoiceReconStatus implements Status
 {
     const PENDING = 'pending';
     const AMOUNT_MISMATCH = 'amount-mismatch';
