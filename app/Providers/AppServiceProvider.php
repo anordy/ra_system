38e4b62b8ff72f9fb27a1bf4216ad316b4ce2f27
mangerere <107620925+mangerere@users.noreply.github.com>
2023-03-31 18:23:06 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,6 +33,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') === 'production') {
+            \URL::forceScheme('https');
+        }
+
         Paginator::useBootstrap();
         Livewire::component('modals', Modals::class);
         Livewire::component('drivers-license.wizard.application-initial-step', ApplicationInitialStep::class);
