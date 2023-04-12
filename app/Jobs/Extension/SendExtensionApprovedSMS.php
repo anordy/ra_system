99eb4db9e969e2c44438f7f6695e21bb61d9f0fe
Meshack Fungo <meshack.fungo@ubx.co.tz>
2023-04-12 21:37:54 +0300

    Update stamp duty form

--- app/Jobs/Extension/SendExtensionApprovedSMS.php
+++ app/Jobs/Extension/SendExtensionApprovedSMS.php
@@ -4,7 +4,6 @@ namespace App\Jobs\Extension;
 
 use App\Http\Controllers\v1\SMSController;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
