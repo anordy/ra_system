85db4d4371f3f2311e873be9dc2afa75918e4336
GeraldNjau <108731600+GeraldMarkNjau@users.noreply.github.com>
2023-04-14 12:01:54 +0300

    filing deadline time configurtion

--- app/Models/SystemSettingCategory.php
+++ app/Models/SystemSettingCategory.php
@@ -14,6 +14,12 @@ class SystemSettingCategory extends Model implements Auditable
 
     public const CERTIFICATESETTINGS_ID = 3;
 
+    public const PASSWORD_POLICY = 'password-policy';
+    public const LOGIN_SETTINGS = 'login-settings';
+    public const CERTIFICATE_SETTINGS = 'certificate-settings';
+    public const FILING_DEADLINE = 'filing-deadline';
+//    public const
+
     public function system_settings(){
         return $this->hasMany(SystemSetting::class);
     }
