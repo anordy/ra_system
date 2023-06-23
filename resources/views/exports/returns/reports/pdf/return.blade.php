4e52d19fab57c0d10e249674e0f6e14af745019a
mangerere <107620925+mangerere@users.noreply.github.com>
2023-06-23 12:40:29 +0300

    Improvement on Return Report

--- resources/views/exports/returns/reports/pdf/return.blade.php
+++ resources/views/exports/returns/reports/pdf/return.blade.php
@@ -48,6 +48,12 @@
             width: 100%;
             border-collapse: collapse;
             background: transparent;
+            table-layout: fixed;
+        }
+
+        table td {
+            word-wrap: break-word;
+            overflow-wrap: break-word;
         }
     </style>
 </head>
@@ -60,37 +66,40 @@
                 <th style="text-align:center;" colspan="15">
                     <strong>ZANZIBAR Revenue Authority</strong><br>
                     {{-- <strong>{{ $title }}</strong><br> --}}
-                    <strong>Report of {{ $parameters['type'] == 'Filing' ? $parameters['filing_report_type'] :
-                        $parameters['payment_report_type'] }} for {{ $parameters['tax_type_name'] }} </strong><br>
-                    <strong>From {{ date("M, d Y", strtotime($parameters['range_start'])) }} To {{ date("M, d Y",
-                        strtotime($parameters['range_end'])) }} </strong><br>
+                    <strong>Report of
+                        {{ $parameters['type'] == 'Filing' ? $parameters['filing_report_type'] : $parameters['payment_report_type'] }}
+                        for {{ $parameters['tax_type_name'] }} </strong><br>
+                    <strong>From {{ date('M, d Y', strtotime($parameters['range_start'])) }} To
+                        {{ date('M, d Y', strtotime($parameters['range_end'])) }} </strong><br>
 
                     <strong>Tax Regions :
-                        @foreach ($parameters['tax_regions'] as $t=>$id)
-                        <span>{{ App\Models\TaxRegion::find($id)->name }}</span>
-                        @if (count($parameters['tax_regions']) > 1)
-                        @if($t == (end($parameters['tax_regions'] )-2))
-                        and @else ,
-                        @endif
-                        @endif &nbsp;
+                        @foreach ($parameters['tax_regions'] as $t => $id)
+                            <span>{{ App\Models\TaxRegion::find($id)->name }}</span>
+                            @if (count($parameters['tax_regions']) > 1)
+                                @if ($t == end($parameters['tax_regions']) - 2)
+                                    and
+                                @else
+                                    ,
+                                @endif
+                            @endif &nbsp;
                         @endforeach
                     </strong><br>
 
                     Location:
                     @if ($parameters['region'] == 'all')
-                    <strong> Pemba and Unguja </strong><br>
+                        <strong> Pemba and Unguja </strong><br>
                     @else
-                    <strong>{{ App\Models\Region::find($parameters['region'])->name }}</strong><br>
+                        <strong>{{ App\Models\Region::find($parameters['region'])->name }}</strong><br>
                     @endif
 
 
                     @if ($parameters['district'] != 'all')
-                    <strong>District: {{ App\Models\District::find($parameters['district'])->name }}</strong><br>
+                        <strong>District: {{ App\Models\District::find($parameters['district'])->name }}</strong><br>
                     @endif
 
 
                     @if ($parameters['ward'] != 'all')
-                    <strong> Ward: {{ App\Models\Ward::find($parameters['ward'])->name }}</strong><br>
+                        <strong> Ward: {{ App\Models\Ward::find($parameters['ward'])->name }}</strong><br>
                     @endif
 
                     <strong>Total Number of Records: {{ $records->count() }} </strong>
@@ -160,78 +169,79 @@
         </thead>
         <tbody>
             @foreach ($records as $index => $record)
-            <tr>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{ $index + 1 }}
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{ $record->business->name ?? '-' }}
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{ $record->location->name ?? '-' }}
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{ $record->taxType->name ?? '-' }}
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    @if ($record->taxType->code == 'lumpsum-payment')
-                    {{ \App\Models\Returns\LumpSum\LumpSumReturn::where('id',$record->return_id)->first()->quarter_name
-                    ?? '-'}}
-                    @else
-                    {{ $record->financialMonth->name ?? '-' }}
-                    @endif
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{ $record->taxpayer->full_name ?? '-' }}
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{ $record->currency ?? '-' }}
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{ $record->principal===null?'-':number_format($record->principal, 2) }}
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{$record->interest===null?'-':number_format($record->interest, 2) }}
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{$record->penalty===null?'-':number_format($record->penalty, 2) }}
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{$record->total_amount===null?'-':number_format($record->total_amount, 2) }}
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{$record->outstanding_amount===null?'-':number_format($record->outstanding_amount, 2) }}
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{ date('d/m/Y', strtotime($record->created_at)) }}
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{$record->filing_due_date==null?'-': date('d/m/Y', strtotime($record->filing_due_date)) }}
-                </td>
-
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{ $record->paid_at==null?'-':date('d/m/Y', strtotime($record->paid_at)) }}
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    {{ $record->payment_due_date==null?'-':date('d/m/Y', strtotime($record->payment_due_date)) }}
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    @if ($record->created_at > $record->filing_due_date )
-                    Late Filing
-                    @else
-                    In-Time Filing
-                    @endif
-                </td>
-                <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
-                    @if($record->paid_at > $record->payment_due_date)
-                    Late Payment
-                    @elseif($record->paid_at < $record->payment_due_date)
-                        In-Time Payment
+                <tr>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ $index + 1 }}
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ $record->business->name ?? '-' }}
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ $record->location->name ?? '-' }}
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ $record->taxType->name ?? '-' }}
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        @if ($record->taxType->code == 'lumpsum-payment')
+                            {{ \App\Models\Returns\LumpSum\LumpSumReturn::where('id', $record->return_id)->first()->quarter_name ?? '-' }}
                         @else
-                        Not Paid
+                            {{ $record->financialMonth->name ?? '-' }}
                         @endif
-                </td>
-            </tr>
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ $record->taxpayer->full_name ?? '-' }}
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ $record->currency ?? '-' }}
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ $record->principal === null ? '-' : number_format($record->principal, 2) }}
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ $record->interest === null ? '-' : number_format($record->interest, 2) }}
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ $record->penalty === null ? '-' : number_format($record->penalty, 2) }}
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ $record->total_amount === null ? '-' : number_format($record->total_amount, 2) }}
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ $record->outstanding_amount === null ? '-' : number_format($record->outstanding_amount, 2) }}
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ date('d/m/Y', strtotime($record->created_at)) }}
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ $record->filing_due_date == null ? '-' : date('d/m/Y', strtotime(\Carbon\Carbon::create($record->filing_due_date)->addMonth())) }}
+                    </td>
+
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ $record->paid_at == null ? '-' : date('d/m/Y', strtotime($record->paid_at)) }}
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        {{ $record->curr_payment_due_date == null ? '-' : date('d/m/Y', strtotime($record->curr_payment_due_date)) }}
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        @if ($record->created_at > \Carbon\Carbon::create($record->filing_due_date)->addMonth())
+                            Late Filing
+                        @else
+                            In-Time Filing
+                        @endif
+                    </td>
+                    <td style="text-align:center;border-collapse:collapse;border: 1px solid black;">
+                        @if (!$record->paid_at)
+                            Not Paid
+                        @elseif($record->paid_at > $record->curr_payment_due_date)
+                            Late Payment
+                        @elseif($record->paid_at <= $record->curr_payment_due_date)
+                            Paid
+                        @else
+                            -
+                        @endif
+                    </td>
+                </tr>
             @endforeach
         </tbody>
     </table>
