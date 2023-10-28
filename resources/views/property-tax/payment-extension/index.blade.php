46c5cdefdde210e97935999ede2ba720cf810d8a
Meshack Fungo <meshack.fungo@ubx.co.tz>
2023-10-28 11:21:30 +0300

    Extend initial

--- resources/views/property-tax/payment-extension/index.blade.php
+++ resources/views/property-tax/payment-extension/index.blade.php
@@ -6,6 +6,12 @@
     <div class="card">
         <div class="card-header">
             <h5 class="text-uppercase">Property Payment Extension Requests</h5>
+            <div class="card-tools">
+                        <button class="btn btn-info btn-sm"
+                                onclick="Livewire.emit('showModal', 'property-tax.payment-extension.extend-due-date-modal')">
+                            <i class="fa fa-plus-circle"></i>Extend Due Date
+                        </button>
+            </div>
         </div>
         <div class="card-body">
             @livewire('property-tax.payment-extension.payment-extension-table')
