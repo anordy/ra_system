109406dd5ad63995d4a6047a11eb4c39b9249866
Meshack Fungo <meshack.fungo@ubx.co.tz>
2024-02-05 15:00:41 +0300

    Modify page 1: Business Reg

--- resources/views/business/certificate.blade.php
+++ resources/views/business/certificate.blade.php
@@ -30,70 +30,72 @@
             top: 32.3%;
         }
         .taxpayer-name {
-            font-size: 1.4em;
-            left: 3%;
-            top: 46%;
+            font-size: 1.3em;
+            left: 10%;
+            top: 42.75%;
         }
         .taxpayer {
-            font-size: 1.6em;
-            top: 40.2%;
+            font-size: 1.4em;
+            top: 38%;
         }
         .trading-as {
             font-size: 0.8em;
-            top: 44%;
+            top: 41%;
         }
         .reg-no {
-            font-size: 1.7em;
-            top: 59%;
+            font-size: 2em;
+            top: 51%;
         }
         .reg-no-alt {
-            font-size: 1.5em;
-            top: 52.5%;
+            font-size: 2em;
+            top: 51%;
         }
         .vrn-no {
             font-size: .8em;
-            top: 55.5%;
+            top: 55%;
         }
         .tax-types {
             font-size: 1.1em;
-            top: 65%;
-            left: 38.4%;
-            position: absolute;
-            text-transform: uppercase;
-            font-weight: bold;
+            top: 60.3%;
+            left: -23%;
         }
         .location {
             font-size: 1.1em;
-            top: 70.8%;
-            left: 10.8%;
+            top: 66.2%;
+            left: -8.0%;
         }
         .zra-location {
             font-size: 1.1em;
-            top: 73.5%;
-            left: -12.4%;
+            top: 69.2%;
+            left: -24%;
+        }
+        .tax-region {
+            font-size: 1.1em;
+            top: 72.1%;
+            left: -22%;
         }
         .commencing-date {
             font-size: 1.1em;
-            top: 67.7%;
-            left: -7.5%;
+            top: 63.25%;
+            left: -12.0%;
         }
         .on-hand-date {
             font-size: 0.8em;
-            top: 92.2%;
-            left: 9%;
+            top: 85.7%;
+            left: 15%;
         }
         .commissioner-signature {
-            top: 79%;
+            top: 73%;
             position: absolute;
             text-transform: uppercase;
             font-weight: bold;
             width: 100%;
             padding-left: 70px;
             padding-right: 70px;
-            left: 65%;
+            left: 40.5%;
         }
         .commissioner-name {
-            top: 86.5%;
+            top: 80.5%;
             position: absolute;
             text-transform: capitalize;
             font-weight: bold;
@@ -102,25 +104,24 @@
             padding-left: 70px;
             padding-right: 70px;
             margin-left: 55px;
-            left: 60%;
+            left: 37.5%;
         }
         .commissioner-title {
-            top: 88%;
+            top: 82%;
             position: absolute;
-            text-transform: uppercase;
             font-weight: bold;
             font-size: 20px;
             width: 100%;
             padding-left: 70px;
             padding-right: 70px;
             margin-left: 55px;
-            left: 59.5%;
+            left: 35.5%;
         }
         .qr-code {
             overflow: hidden;
             position:absolute;
-            top: 79%;
-            right: 78%;
+            top: 77%;
+            right: 76%;
             background: white;
             border-radius: 5px;
             height: 180px;
@@ -149,7 +150,7 @@
         @endif
         <span class="embed taxpayer">{{ ($location->business->taxpayer_name ? $location->business->taxpayer_name : $location->business->name) ?? '' }}</span>
         @if(isset($location->business->name) && isset($location->business->taxpayer_name) && strtolower(trim($location->business->name)) != strtolower(trim($location->business->taxpayer_name)))
-        <span class="embed trading-as">T/A {{ $location->business->name }}</span>
+            <span class="embed trading-as">T/A {{ $location->business->name }}</span>
         @endif
         <span class="embed taxpayer-name">{{ getFormattedTinNo($location) ?? '' }}</span>
         @if($location->vrn)
@@ -158,12 +159,15 @@
         @else
             <span class="embed reg-no">{{ $location->business->ztn_number ?? '' }}</span>
         @endif
-        <span class="tax-types">{{ $tax->name == 'VAT' ? 'VALUE ADDED TAX' : $tax->name }}</span>
+        <span class="embed tax-types">{{ $tax->name == 'VAT' ? 'VALUE ADDED TAX' : $tax->name }}</span>
         <span class="embed location">
-            {{ $location->street->name }}-{{ $location->ward->name }} - {{ $location->region->location }}
+            {{ $location->street->name }} - {{ $location->ward->name }}
         </span>
         <span class="embed zra-location">
             {{ $location->region->location }}
+        </span>
+        <span class="embed tax-region">
+            {{ $location->taxRegion->name }}
         </span>
         <span class="embed commencing-date">
             {{ $location->date_of_commencing->format('d F Y') }}
@@ -182,6 +186,9 @@
         </span>
         <div class="qr-code">
             <img class="img-fluid" src="{{ $dataUri }}" style="height: 189px">
+        </div>
+        <div style="page-break-before: always">
+            hi
         </div>
     </body>
 </html>
