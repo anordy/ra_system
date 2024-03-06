150d57935cabc16fba7e0e7d54f68222b7ae70a9
mangerere <107620925+mangerere@users.noreply.github.com>
2024-03-06 12:30:44 +0300

    Update CustomMessage.php

--- app/Enum/CustomMessage.php
+++ app/Enum/CustomMessage.php
@@ -4,9 +4,10 @@ namespace App\Enum;
 
 use ReflectionClass;
 
-class CustomResponse implements Status
+class CustomMessage implements Status
 {
     const ERROR = 'Something went wrong, please contact your system administrator for support.';
+    const ARE_YOU_SURE = 'Are you sure you want to complete this action?';
 
 
     static function getConstants(): array
