8f7b28837a74555c13873612fdc4201217493488
Meshack Fungo <meshack.fungo@ubx.co.tz>
2024-04-04 13:57:19 +0300

    add: report code review fixes

--- app/Http/Controllers/Reports/Dispute/DisputeReportController.php
+++ app/Http/Controllers/Reports/Dispute/DisputeReportController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers\Reports\Dispute;
 
+use App\Enum\CustomMessage;
 use App\Http\Controllers\Controller;
 use App\Models\TaxType;
-use App\Traits\AssessmentReportTrait;
 use App\Traits\DisputeReportTrait;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
 use PDF;
-use Illuminate\Http\Request;
 
 class DisputeReportController extends Controller
 {
@@ -26,8 +26,15 @@ class DisputeReportController extends Controller
         if (!Gate::allows('managerial-report-preview')) {
             abort(403);
         }
-        $parameters = json_decode(decrypt($parameters), true);
-        return view('reports.dispute.preview', compact('parameters'));
+
+        try {
+            $parameters = json_decode(decrypt($parameters), true);
+            return view('reports.dispute.preview', compact('parameters'));
+        } catch (\Exception $exception){
+            Log::error($exception);
+            session()->flash('error', CustomMessage::error());
+            return back();
+        }
     }
 
     public function exportDisputeReportPdf($parameters)
@@ -35,26 +42,32 @@ class DisputeReportController extends Controller
         if (!Gate::allows('managerial-report-pdf')) {
             abort(403);
         }
-        $parameters = json_decode(decrypt($parameters), true);
-        $records = $this->getRecords($parameters);
-        if ($parameters['tax_type_id'] == 'all') {
-            $tax_type_name = 'All';
 
-        } else {
-            $tax_type_name = TaxType::findOrFail($parameters['tax_type_id'])->name;
-        }
+        try {
+            $parameters = json_decode(decrypt($parameters), true);
+            $records = $this->getRecords($parameters);
+            if ($parameters['tax_type_id'] == 'all') {
+                $tax_type_name = 'All';
+            } else {
+                $tax_type_name = TaxType::findOrFail($parameters['tax_type_id'])->name;
+            }
 
-        if ($parameters['year'] == 'all') {
-            $fileName = $tax_type_name. '_' . 'Disputes' . '.pdf';
-            $title = 'Notice of Dispute'. ' For ' . $tax_type_name;
-        } else {
-            $fileName = $tax_type_name. '_' . 'Disputes' . ' - ' . $parameters['year'] . '.pdf';
-            $title = 'Disputes' . ' For ' . $tax_type_name . '-' . $parameters['year'];
+            if ($parameters['year'] == 'all') {
+                $fileName = $tax_type_name. '_' . 'Disputes' . '.pdf';
+                $title = 'Notice of Dispute'. ' For ' . $tax_type_name;
+            } else {
+                $fileName = $tax_type_name. '_' . 'Disputes' . ' - ' . $parameters['year'] . '.pdf';
+                $title = 'Disputes' . ' For ' . $tax_type_name . '-' . $parameters['year'];
+            }
+            $records = $records->get();
+            $pdf = PDF::loadView('exports.disputes.reports.pdf.dispute', compact('records', 'title', 'parameters'));
+            $pdf->setPaper('a4', 'landscape');
+            $pdf->setOption(['dpi' => 150, 'defaultFont' => 'sans-serif']);
+            return $pdf->download($fileName);
+        } catch (\Exception $exception){
+            Log::error($exception);
+            session()->flash('error', CustomMessage::error());
+            return back();
         }
-        $records = $records->get();
-        $pdf = PDF::loadView('exports.disputes.reports.pdf.dispute', compact('records', 'title', 'parameters'));
-        $pdf->setPaper('a4', 'landscape');
-        $pdf->setOption(['dpi' => 150, 'defaultFont' => 'sans-serif']);
-        return $pdf->download($fileName);
     }
 }
