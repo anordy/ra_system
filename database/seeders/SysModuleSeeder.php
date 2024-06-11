aa66f6c6e7c83fe18ce32297825e8c4810223e8a
costantine-tarimo <112546624+costantine-tarimo@users.noreply.github.com>
2024-06-11 08:00:24 +0300

    fix: changed license spelling

--- database/seeders/SysModuleSeeder.php
+++ database/seeders/SysModuleSeeder.php
@@ -47,7 +47,7 @@ class SysModuleSeeder extends Seeder
             ['id' => 30, 'code' => 'legal-cases', 'name' => 'Legal Cases'],
             ['id' => 31, 'code' => 'property-tax', 'name' => 'Property Tax'],
 
-            ['id' => 31, 'code' => 'vfm-integration', 'name' => 'VFMS Integration'],
+            ['id' => 32, 'code' => 'vfm-integration', 'name' => 'VFMS Integration'],
 
 
         ];
