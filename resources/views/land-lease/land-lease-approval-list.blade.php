5645bfb3b180f3a4cf27b24e03f1e2e9e33ceceb
costantine-tarimo <112546624+costantine-tarimo@users.noreply.github.com>
2024-06-19 11:16:33 +0300

    added tab for land lease registration approval

--- resources/views/land-lease/land-lease-approval-list.blade.php
+++ resources/views/land-lease/land-lease-approval-list.blade.php
@@ -6,21 +6,29 @@
     <div class="rounded-0">
         <div class="card-body">
             <nav class="nav nav-tabs mt-0 border-top-0">
-                <a href="#tab1" class="nav-item nav-link font-weight-bold active">{{ __('Lease Partial Payments Approvals')
-            }}</a>
+                <a href="#tab1" class="nav-item nav-link font-weight-bold active">{{ __('Lease Registration Approvals')
+                }}</a>
+                <a href="#tab2" class="nav-item nav-link font-weight-bold ">{{ __('Lease Partial Payments
+                Approvals')}}</a>
                 @can('land-lease-approve-currency-change-application')
-                    <a href="#tab2"
+                    <a href="#tab3"
                        class="nav-item nav-link font-weight-bold">{{ __('Lease Currency Change Approvals') }}</a>
                 @endcan
+
             </nav>
             <div class="tab-content px-2 card mb-1 shadow-none border rounded-0">
                 <div id="tab1" class="tab-pane fade m-2 show active">
+                    <div>
+                        <livewire:land-lease.land-lease-registration-approve-list/>
+                    </div>
+                </div>
+                <div id="tab2" class="tab-pane fade m-2 show">
                     <div>
                         <livewire:land-lease.land-lease-approve-list/>
                     </div>
                 </div>
                 @can('land-lease-approve-currency-change-application')
-                    <div id="tab2" class="tab-pane fade m-2">
+                    <div id="tab3" class="tab-pane fade m-2">
                         <livewire:land-lease.lease-currency-approve-list/>
                     </div>
                 @endcan
