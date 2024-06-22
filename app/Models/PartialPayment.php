9a6f63de07aab0a46b0e54314ce9953c5d0d76d7
costantine-tarimo <112546624+costantine-tarimo@users.noreply.github.com>
2024-06-22 11:05:43 +0300

    Add partial payment model

---
+++ app/Models/PartialPayment.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PartialPayment extends Model
+{
+    protected $guarded = [];
+
+    public function landLease()
+    {
+        return $this->belongsTo(LandLease::class, 'payment_id');
+    }
+
+    public function bills()
+    {
+        return $this->morphMany(ZmBill::class, 'billable');
+    }
+
+    public function latestBill()
+    {
+        return $this->morphOne(ZmBill::class, 'billable')->latest();
+    }
+
+    public function getBillAttribute()
+    {
+        return $this->morphMany(ZmBill::class, 'billable')->latest()->first();
+    }
+
+    public function secondLatest()
+    {
+        return $this->morphMany(ZmBill::class, 'billable')->latest();
+    }
+}
