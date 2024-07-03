b9ba0972af83cfdecdb890c92bfa25b17ff58366
mangerere <107620925+mangerere@users.noreply.github.com>
2024-07-03 21:55:52 +0300

    Update Parameter.php

--- app/Models/Parameter.php
+++ app/Models/Parameter.php
@@ -10,4 +10,17 @@ class Parameter extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public const START_DATE = 'start_date';
+    public const END_DATE = 'end_date';
+    public const FINANCIAL_YEAR = 'financial_year';
+    public const FINANCIAL_MONTH = 'f_month';
+    public const TAX_TYPE = 'tax_type';
+    public const REGION = 'region';
+    public const DISTRICT = 'district';
+    public const TAX_REGION = 'tax_region';
+    public const DEPARTMENT = 'department';
+
+    public const REGION_NAME = 'region_name';
+
 }
