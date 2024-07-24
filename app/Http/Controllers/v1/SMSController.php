78cc85cdcf5a1b6b2b42baac5928bedb51635a4f
mangerere <107620925+mangerere@users.noreply.github.com>
2024-07-24 13:41:33 +0300

    Update SMSController.php

--- app/Http/Controllers/v1/SMSController.php
+++ app/Http/Controllers/v1/SMSController.php
@@ -3,10 +3,10 @@
 
 namespace App\Http\Controllers\v1;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Services\ZanMalipo\ZmCore;
 use Exception;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class SMSController extends Controller
@@ -21,38 +21,39 @@ class SMSController extends Controller
         $this->middleware('auth');
     }
 
-    public function initSMS(Request $request) {
-        $send_to  = $request->input('send_to');
+    public function initSMS(Request $request)
+    {
+        $send_to = $request->input('send_to');
         $source = $request->input('source');
         $customer_message = $request->input('customer_message');
 
         return $this->sendSMS($send_to, $source, $customer_message);
     }
 
-    public function sendSMS($send_to, $source, $customer_message) {
+    public function sendSMS($send_to, $source, $customer_message)
+    {
         $apiURL = config('modulesconfig.sms_url');
-        
-        $messages[] = [
-            "text" => $customer_message,
-                "msisdn" => ZmCore::formatPhone($send_to),
-                "source" => $source
-        ];
-        
+
+        if (!$customer_message || !$source || !$send_to) {
+                throw new Exception('Missing customer message, source or recipient number');
+        }
+
         $payload = [
-            "channel" => [
-                "channel" => config('modulesconfig.sms_channel'),
-                "password" => config('modulesconfig.sms_password')
-            ],
-            "messages" => $messages
+            'username' => config('modulesconfig.sms_channel'),
+            'password' => config('modulesconfig.sms_password'),
+            'message' => $customer_message,
+            'senderId' => $source,
+            'phoneNumbers' => [ZmCore::formatPhone($send_to)],
         ];
 
+        Log::error('SENDING-SMS');
+
         $curl = curl_init();
+
         curl_setopt_array($curl, array(
             CURLOPT_URL => $apiURL,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30000,
+            CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
             CURLOPT_POSTFIELDS => json_encode($payload),
@@ -63,13 +64,17 @@ class SMSController extends Controller
         ));
 
         $response = curl_exec($curl);
+
         if (curl_errno($curl)) {
             $err = curl_error($curl);
             Log::error($err);
+            Log::error('END-SENDING-SMS');
             curl_close($curl);
             throw new Exception('SMS sending failed');
         }
         curl_close($curl);
+        Log::error('END-SENDING-SMS');
         return $response;
     }
+
 }
