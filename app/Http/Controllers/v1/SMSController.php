b30d75dedf808bbfe8afcc95f781fdec17adc57d
mangerere <107620925+mangerere@users.noreply.github.com>
2024-08-15 10:46:50 +0300

    add: check for fasthub and webbulk

--- app/Http/Controllers/v1/SMSController.php
+++ app/Http/Controllers/v1/SMSController.php
@@ -3,10 +3,10 @@
 
 namespace App\Http\Controllers\v1;
 
+use App\Enum\SmsProvider;
 use App\Http\Controllers\Controller;
 use App\Services\ZanMalipo\ZmCore;
 use Exception;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class SMSController extends Controller
@@ -21,30 +21,29 @@ class SMSController extends Controller
         $this->middleware('auth');
     }
 
-    public function initSMS(Request $request)
-    {
-        $send_to = $request->input('send_to');
-        $source = $request->input('source');
-        $customer_message = $request->input('customer_message');
-
-        return $this->sendSMS($send_to, $source, $customer_message);
-    }
 
     public function sendSMS($send_to, $source, $customer_message)
     {
-        $apiURL = config('modulesconfig.sms_url');
+        $apiURL = config('modulesconfig.wb_sms_url');
 
         if (!$customer_message || !$source || !$send_to) {
-                throw new Exception('Missing customer message, source or recipient number');
+            throw new Exception('Missing customer message, source or recipient number');
+        }
+
+        if (config('modilesconfig.active_sms_provider') === SmsProvider::FAST_HUB) {
+            $this->sendFHSMS($send_to, $source, $customer_message);
+            return;
         }
 
         $payload = [
-            'username' => config('modulesconfig.sms_channel'),
-            'password' => config('modulesconfig.sms_password'),
+            'username' => config('modulesconfig.wb_sms_channel'),
+            'password' => config('modulesconfig.wb_sms_password'),
             'message' => $customer_message,
             'senderId' => $source,
             'phoneNumbers' => [ZmCore::formatPhone($send_to)],
         ];
+
+        Log::error(json_encode($payload));
 
         Log::error('SENDING-SMS');
 
@@ -76,5 +75,52 @@ class SMSController extends Controller
         Log::error('END-SENDING-SMS');
         return $response;
     }
+
+
+    private function sendFHSMS($send_to, $source, $customer_message)
+    {
+        $apiURL = config('modulesconfig.sms_url');
+
+        $messages[] = [
+            "text" => $customer_message,
+            "msisdn" => ZmCore::formatPhone($send_to),
+            "source" => $source
+        ];
+
+        $payload = [
+            "channel" => [
+                "channel" => config('modulesconfig.sms_channel'),
+                "password" => config('modulesconfig.sms_password')
+            ],
+            "messages" => $messages
+        ];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $apiURL,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30000,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode($payload),
+            CURLOPT_HTTPHEADER => array(
+                "accept: application/json",
+                "content-type: application/json",
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        if (curl_errno($curl)) {
+            $err = curl_error($curl);
+            Log::error($err);
+            curl_close($curl);
+            throw new Exception('SMS sending failed');
+        }
+        curl_close($curl);
+        return $response;
+    }
+
 
 }
