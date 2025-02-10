39ea85e6061f188893f60d279fa809aaeef08962
Nsajigwa Ephraim <56828793+Jigwa15@users.noreply.github.com>
2025-02-10 23:30:00 +0300

    add columns in chassis_number_changes table

---
+++ database/migrations/2025_02_10_232339_add_columns_to_chassis_number_changes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToChassisNumberChangesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('chassis_number_changes', function (Blueprint $table) {
+            $table->string('make')->nullable();
+            $table->string('model')->nullable();
+            $table->string('engine_capacity')->nullable();
+            $table->string('year_of_manufacture')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('chassis_number_changes', function (Blueprint $table) {
+
+        });
+    }
+}
